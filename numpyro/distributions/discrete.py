f1b9c8a45ee50ce8543da73ea1af6233fe019243
Neeraj Pradhan <npradhan@uber.com>
2019-03-04 21:52:25 -0800

    rename to discrete

--- numpyro/distributions/discrete.py
+++ numpyro/distributions/discrete.py
@@ -1,4 +1,4 @@
-# Source code modified from scipy.stats._discrete_distns.py
+# Code modified from scipy.stats._discrete_distns.py
 #
 # Copyright (c) 2001, 2002 Enthought, Inc.
 # All rights reserved.
@@ -45,3 +45,29 @@ class binom_gen(jax_discrete):
         k = np.r_[0:n + 1]
         vals = self._pmf(k, n, p)
         return np.sum(entr(vals), axis=0)
+
+
+class bernoulli_gen(binom_gen):
+    def _rvs(self, p):
+        return binom_gen._rvs(self, 1, p)
+
+    def _argcheck(self, p):
+        return (p >= 0) & (p <= 1)
+
+    def _logpmf(self, x, p):
+        return binom._logpmf(x, 1, p)
+
+    def _pmf(self, x, p):
+        # bernoulli.pmf(k) = 1-p  if k = 0
+        #                  = p    if k = 1
+        return binom._pmf(x, 1, p)
+
+    def _stats(self, p):
+        return binom._stats(1, p)
+
+    def _entropy(self, p):
+        return entr(p) + entr(1 - p)
+
+
+bernoulli = bernoulli_gen(b=1, name='bernoulli')
+binom = binom_gen(name='binom')
