47e76ecab6dad8d9a46f4121188b2069de1b0306
fehiepsi <fehiepsi@gmail.com>
2019-03-14 12:53:23 -0400

    mark xfail for uniform distribution

--- test/test_distributions.py
+++ test/test_distributions.py
@@ -80,8 +80,7 @@ def test_discrete_shape(jax_dist, dist_args, prepend_shape):
     if prepend_shape is not None:
         shape = prepend_shape + lax.broadcast_shapes(*[np.shape(arg) for arg in dist_args])
         expected_shape = np.shape(sp_dist.rvs(*dist_args, size=shape))
-        assert_allclose(np.shape(jax_dist.rvs(*dist_args, size=shape, random_state=rng)),
-                        expected_shape)
+        assert np.shape(jax_dist.rvs(*dist_args, size=shape, random_state=rng)) == expected_shape
 
 
 @pytest.mark.parametrize('jax_dist', [
@@ -106,8 +105,10 @@ def test_sample_gradient(jax_dist, loc, scale):
         return jax_dist.rvs(*args, loc=loc, scale=scale, random_state=rng).sum()
 
     assert len(grad(fn)(args, loc, scale)) == jax_dist.numargs
-    assert grad(fn, 1)(args, loc, scale) == loc * reduce(mul, expected_shape[:len(expected_shape) - len(np.shape(loc))], 1.)
-    assert onp.allclose(grad(fn, 2)(args, loc, scale), jax_dist.rvs(*args, size=expected_shape, random_state=rng))
+    assert_allclose(grad(fn, 1)(args, loc, scale),
+                    loc * reduce(mul, expected_shape[:len(expected_shape) - np.ndim(loc)], 1.))
+    assert_allclose(grad(fn, 2)(args, loc, scale),
+                    jax_dist.rvs(*args, size=expected_shape, random_state=rng))
 
 
 @pytest.mark.parametrize('jax_dist', [
@@ -117,7 +118,10 @@ def test_sample_gradient(jax_dist, loc, scale):
     dist.gamma,
     dist.lognorm,
     dist.norm,
-    dist.uniform,
+    pytest.param(dist.uniform,
+                 marks=pytest.mark.xfail(
+                     reason="jax.scipy.uniform.logpdf is not correctly implemented, "
+                     "see https://github.com/google/jax/pull/510")),
 ], ids=lambda jax_dist: jax_dist.name)
 @pytest.mark.parametrize('loc_scale', [
     (),
@@ -126,11 +130,11 @@ def test_sample_gradient(jax_dist, loc, scale):
     (1., np.array([1., 2.])),
 ])
 def test_logprob(jax_dist, loc_scale):
-    rng = random.PRNGKey(2)
+    rng = random.PRNGKey(0)
     args = (1,) * jax_dist.numargs + loc_scale
     samples = jax_dist.rvs(*args, random_state=rng)
     sp_dist = getattr(sp, jax_dist.name)
-    assert np.allclose(jax_dist.logpdf(samples, *args), sp_dist.logpdf(samples, *args))
+    assert_allclose(jax_dist.logpdf(samples, *args), sp_dist.logpdf(samples, *args))
 
 
 @pytest.mark.parametrize('jax_dist, dist_args', [
@@ -179,8 +183,8 @@ def test_standard_gamma_shape(alpha, shape):
 def test_standard_gamma_stats(alpha):
     rng = random.PRNGKey(0)
     z = standard_gamma(rng, np.full((1000,), alpha))
-    assert np.allclose(np.mean(z), alpha, rtol=0.06)
-    assert np.allclose(np.var(z), alpha, rtol=0.2)
+    assert_allclose(np.mean(z), alpha, rtol=0.06)
+    assert_allclose(np.var(z), alpha, rtol=0.2)
 
 
 @pytest.mark.parametrize("alpha", [1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4])
@@ -195,4 +199,4 @@ def test_standard_gamma_grad(alpha):
     pdf = sp.gamma.pdf(z, alpha)
     expected_grad = -cdf_dot / pdf
 
-    assert np.allclose(actual_grad, expected_grad, rtol=0.0005)
+    assert_allclose(actual_grad, expected_grad, rtol=0.0005)
