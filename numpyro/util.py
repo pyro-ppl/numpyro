374bf0b52cad33e23282919df8742a45390f4272
fehiepsi <fehiepsi@gmail.com>
2019-03-21 12:35:28 -0400

    fix turning condition

--- numpyro/util.py
+++ numpyro/util.py
@@ -309,13 +309,13 @@ def _is_turning(inverse_mass_matrix, r_left, r_right, r_sum):
         v_left = np.matmul(inverse_mass_matrix, r_left)
         v_right = np.matmul(inverse_mass_matrix, r_right)
     elif inverse_mass_matrix.ndim == 1:
-        v_left = np.dot(inverse_mass_matrix, r_left)
-        v_right = np.dot(inverse_mass_matrix, r_right)
+        v_left = np.mul(inverse_mass_matrix, r_left)
+        v_right = np.mul(inverse_mass_matrix, r_right)
 
     # This implements dynamic termination criterion (ref [2], section A.4.2).
     turning_at_left = np.dot(v_left, r_sum - r_left) <= 0
     turning_at_right = np.dot(v_right, r_sum - r_right) <= 0
-    return np.all(turning_at_left | turning_at_right)
+    return turning_at_left | turning_at_right
 
 
 @partial(jit, static_argnums=(3,))
