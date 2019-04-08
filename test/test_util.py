2385d35f427b2d300b7db1f7513efc9b6bb550ea
Neeraj Pradhan <npradhan@uber.com>
2019-04-07 22:46:24 -0700

    Add a transform function to tscan

--- test/test_util.py
+++ test/test_util.py
@@ -1,4 +1,5 @@
 import pytest
+from jax.test_util import check_eq
 from numpy.testing import assert_allclose
 
 import jax.numpy as np
@@ -15,18 +16,32 @@ def test_tscan(prims_disabled):
         return tree_map(lambda x: (x + y) * z, tree)
 
     Tree = laxtuple("Tree", ["x", "y", "z"])
+    OutTree = laxtuple("OutTree", ["x", "z"])
     a = Tree(np.array([1., 2.]),
              np.array(3., dtype=np.float32),
              np.array(4., dtype=np.float32))
     bs = (np.array([1., 2., 3., 4.]),
           np.array([4., 3., 2., 1.]))
 
-    expected_tree = lax.scan(f, a, bs)
+    scan_tree = lax.scan(f, a, bs)
+    expected_tree = OutTree(scan_tree.x, scan_tree.z)
     with optional(prims_disabled, control_flow_prims_disabled()):
-        actual_tree = tscan(f, a, bs, fields=(0, 2))
-    assert_allclose(actual_tree.x, expected_tree.x)
-    assert_allclose(actual_tree.z, expected_tree.z)
-    assert actual_tree.y is None
+        actual_tree = tscan(f, a, bs, transform=lambda a: OutTree(a.x, a.z))
+    check_eq(actual_tree, expected_tree)
+
+
+@pytest.mark.parametrize('prims_disabled', [True, False])
+def test_tscan_dict(prims_disabled):
+    def f(x, y):
+        return {'i': x['i'] + y['i'], 'j': x['j'] - y['j']}
+
+    a = {'i': np.array([0.]), 'j': np.array([1.])}
+    bs = {'i': np.array([1., 2., 3.]), 'j': np.array([2., 3., 6.])}
+    scan_tree = lax.scan(f, a, bs)
+    expected_tree = {'i': scan_tree['i']}
+    with optional(prims_disabled, control_flow_prims_disabled()):
+        actual_tree = tscan(f, a, bs, transform=lambda a: {'i': a['i']})
+    check_eq(actual_tree, expected_tree)
 
 
 def test_scan_prims_disabled():
