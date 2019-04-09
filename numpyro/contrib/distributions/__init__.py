7bac4592c30322f1a6e7ff04b2fd21b70c951aa1
Neeraj Pradhan <npradhan@uber.com>
2019-04-09 16:59:13 -0700

    Refactor distribution classes to remove dependency on scipy

---
+++ numpyro/contrib/distributions/__init__.py
@@ -0,0 +1,8 @@
+from numpyro.contrib.distributions.continuous import Normal, Uniform
+from numpyro.contrib.distributions.discrete import Bernoulli
+
+__all__ = [
+    'Bernoulli',
+    'Normal',
+    'Uniform',
+]
