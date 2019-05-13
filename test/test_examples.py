ef449b24de63e5377cd3d123c602c04c70e4546b
Neeraj Pradhan <npradhan@uber.com>
2019-05-13 15:03:39 -0700

    make covtype faster

--- test/test_examples.py
+++ test/test_examples.py
@@ -10,7 +10,7 @@ EXAMPLES_DIR = os.path.join(os.path.dirname(TESTS_DIR), 'examples')
 
 EXAMPLES = [
     'baseball.py --num-samples 100 --num-warmup 100',
-    'covtype.py --num-samples 100',
+    'covtype.py --algo hmc --num-samples 10',
     'minipyro.py',
     'stochastic_volatility.py --num-samples 100 --num-warmup 100',
     'ucbadmit.py',
