c02c0eca8d589aef39524800cdf22e2aca70168f
Neeraj Pradhan <npradhan@uber.com>
2019-05-20 15:55:18 -0700

    revert

--- test/test_mcmc.py
+++ test/test_mcmc.py
@@ -108,7 +108,7 @@ def test_dirichlet_categorical(algo):
 
 def test_change_point():
     # Ref: https://forum.pyro.ai/t/i-dont-understand-why-nuts-code-is-not-working-bayesian-hackers-mail/696
-    warmup_steps, num_samples = 1000, 2000
+    warmup_steps, num_samples = 500, 1000
 
     def model(data):
         alpha = 1 / np.mean(data)
@@ -126,7 +126,8 @@ def test_change_point():
         12,  35,  17,  23,  17,   4,   2,  31,  30,  13,  27,   0,  39,  37,
         5,  14,  13,  22,
     ])
-    init_params, potential_fn, constrain_fn = initialize_model(random.PRNGKey(2), model, count_data)
+    init_params, potential_fn, constrain_fn = initialize_model(random.PRNGKey(2), model, count_data,
+                                                               init_strategy='prior')
     init_kernel, sample_kernel = hmc(potential_fn)
     hmc_state = init_kernel(init_params, num_warmup=warmup_steps)
     hmc_states = fori_collect(num_samples, sample_kernel, hmc_state,
