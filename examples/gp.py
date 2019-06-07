1cb01eb671eea8f9272dbd17486346cc8c4d4c20
fehiepsi <fehiepsi@gmail.com>
2019-06-07 12:33:55 -0400

    revert default num-data to 25

--- examples/gp.py
+++ examples/gp.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Gaussian Process example")
     parser.add_argument("-n", "--num-samples", nargs="?", default=1000, type=int)
     parser.add_argument("--num-warmup", nargs='?', default=1000, type=int)
-    parser.add_argument("--num-data", nargs='?', default=400, type=int)
+    parser.add_argument("--num-data", nargs='?', default=25, type=int)
     parser.add_argument("--device", default='cpu', type=str, help='use "cpu" or "gpu".')
     args = parser.parse_args()
     main(args)
