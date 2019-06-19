3d4db33d32a52784c3f6825ebb96c9446896bda8
Martin Jankowiak <jankowiak@uber.com>
2019-06-18 18:53:24 -0700

    add jacobian test

--- test/test_iaf.py
+++ test/test_iaf.py
@@ -3,7 +3,7 @@ import numpy as onp
 from numpy.testing import assert_allclose
 from numpyro.contrib.nn import AutoRegressiveNN
 from numpyro.distributions import InverseAutoRegressiveTransform
-from jax import random
+from jax import random, jacfwd
 
 
 @pytest.mark.parametrize('input_dim', [5, 7])
@@ -22,8 +22,24 @@ def test_auto_reg_nn(input_dim, hidden_dims):
     x = onp.random.rand(*input_shape)
     y = iaf(x)
     inv = iaf.inv(y)
-
     assert_allclose(x, inv, atol=1e-5)
 
-    # smoketest
-    iaf.log_abs_det_jacobian(x, y)
+    # test jacobians
+    x = onp.random.rand(*input_shape[-1:])
+    jac = jacfwd(iaf)(x)
+
+    # permute jacobian as necessary
+    permuted_jac = onp.zeros(jac.shape)
+    perm = arn.permutation
+
+    for j in range(input_dim):
+        for k in range(input_dim):
+            permuted_jac[..., j, k] = jac[..., perm[j], perm[k]]
+
+    # make sure jacobians are triangular
+    assert onp.sum(onp.abs(onp.triu(permuted_jac, 1))) == 0.00
+
+    # make sure iaf.log_abs_det_jacobian is correct
+    ldj = iaf.log_abs_det_jacobian(x, y)
+    assert_allclose(ldj, onp.sum(onp.log(onp.diag(permuted_jac))), atol=1e-5)
+
