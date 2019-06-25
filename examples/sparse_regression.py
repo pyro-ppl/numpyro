4c395598d9c2a0f6f13cb4b257bc6a0c57701c85
martinjankowiak <martinjankowiak@users.noreply.github.com>
2019-06-25 08:32:15 -0700

    add sparse linear regression example (#219)
    * initial commit
    
    * single chain
    
    * tweak hypers
    
    * tweak hyperparameters
    
    * address comments

---
+++ examples/sparse_regression.py
@@ -0,0 +1,203 @@
+import argparse
+import time
+
+import numpy as onp
+
+import jax
+from jax import vmap
+from jax.config import config as jax_config
+import jax.numpy as np
+import jax.random as random
+
+import numpyro.distributions as dist
+from numpyro.handlers import sample
+from numpyro.hmc_util import initialize_model
+from numpyro.mcmc import mcmc
+
+
+"""
+We demonstrate how to do (fully Bayesian) sparse linear regression using the
+approach described in [1]. This approach is particularly suitable for situations
+with many feature dimensions (large P) but not too many datapoints (small N).
+In particular we consider a quadratic regressor of the form:
+
+f(X) = constant + theta_i X_i + theta_ij X_i X_j + observation noise
+
+References
+[1] The Kernel Interaction Trick: Fast Bayesian Discovery of Pairwise
+    Interactions in High Dimensions.
+    Raj Agrawal, Jonathan H. Huggins, Brian Trippe, Tamara Broderick
+    https://arxiv.org/abs/1905.06501
+"""
+
+
+def dot(X, Z):
+    return np.dot(X, Z[..., None])[..., 0]
+
+
+# The kernel that corresponds to our quadratic regressor.
+# (Note that we put the same prior on theta_ij for i=j as i!=j)
+def kernel(X, Z, eta1, eta2, c):
+    eta1sq = np.square(eta1)
+    eta2sq = np.square(eta2)
+    k1 = 0.5 * eta2sq * np.square(1.0 + dot(X, Z))
+    k2 = 0.5 * eta2sq * dot(np.square(X), np.square(Z))
+    k3 = (eta1sq - eta2sq) * dot(X, Z)
+    k4 = np.square(c) - 0.5 * eta2sq
+    return k1 + k2 + k3 + k4
+
+
+# Most of the model code is concerned with constructing the sparsity inducing prior.
+def model(X, Y, hypers):
+    S, P, N = hypers['expected_sparsity'], X.shape[1], X.shape[0]
+
+    sigma = sample("sigma", dist.HalfNormal(hypers['alpha3']))
+    phi = sigma * (S / np.sqrt(N)) / (P - S)
+    eta1 = sample("eta1", dist.HalfCauchy(phi))
+
+    msq_inv = sample("msq_inv", dist.Gamma(hypers['alpha1'], hypers['beta1']))
+    msq = 1.0 / msq_inv
+    xisq_inv = sample("xisq_inv", dist.Gamma(hypers['alpha2'], hypers['beta2']))
+
+    eta2 = np.square(eta1) * msq_inv / np.sqrt(xisq_inv)
+
+    lam = sample("lambda", dist.HalfCauchy(np.ones(P)))
+    kappa = np.sqrt(msq) * lam / np.sqrt(msq + np.square(eta1 * lam))
+
+    # sample observation noise
+    prec_obs = sample("prec_obs", dist.Gamma(hypers['alpha_obs'], hypers['beta_obs']))
+
+    # compute kernel
+    kX = kappa * X
+    k = kernel(kX, kX, eta1, eta2, hypers['c']) + np.eye(N) / prec_obs
+    assert k.shape == (N, N)
+
+    # sample Y according to the standard gaussian process formula
+    sample("Y", dist.MultivariateNormal(loc=np.zeros(X.shape[0]), covariance_matrix=k),
+           obs=Y)
+
+
+# Compute the mean and variance of coefficient theta_i (where i = dimension) for a
+# MCMC sample (eta1, xi, ...). Compare to theorem 5.1 in reference [1].
+def compute_mean_variance(X, Y, dimension, msq_inv, lam, eta1, xisq_inv, c, prec_obs):
+    P, N = X.shape[1], X.shape[0]
+
+    probe = np.zeros((2, P))
+    probe = jax.ops.index_update(probe, jax.ops.index[:, dimension], np.array([0.5, -0.5]))
+
+    eta2 = np.square(eta1) * msq_inv / np.sqrt(xisq_inv)
+    msq = 1.0 / msq_inv
+    kappa = np.sqrt(msq) * lam / np.sqrt(msq + np.square(eta1 * lam))
+
+    kX = kappa * X
+    kprobe = kappa * probe
+
+    k_xx = kernel(kX, kX, eta1, eta2, c) + np.eye(N) / prec_obs
+    k_xx_inv = np.linalg.inv(k_xx)
+    k_probeX = kernel(kprobe, kX, eta1, eta2, c)
+    k_prbprb = kernel(kprobe, kprobe, eta1, eta2, c)
+
+    mu = np.matmul(k_probeX, np.matmul(k_xx_inv, Y))
+    mu = mu[0] - mu[1]
+
+    var = k_prbprb - np.matmul(k_probeX, np.matmul(k_xx_inv, np.transpose(k_probeX)))
+    var = np.matmul(var, np.array([1, -1]))
+    var = var[0] - var[1]
+
+    return mu, var
+
+
+# Helper function for doing hmc inference
+def run_inference(model, args, rng, X, Y, hypers):
+    if args.num_chains > 1:
+        rng = random.split(rng, args.num_chains)
+    init_params, potential_fn, constrain_fn = initialize_model(rng, model, X, Y, hypers)
+    start = time.time()
+    samples = mcmc(args.num_warmup, args.num_samples, init_params, num_chains=args.num_chains,
+                   sampler='hmc', potential_fn=potential_fn, constrain_fn=constrain_fn)
+    print('\nMCMC elapsed time:', time.time() - start)
+    return samples
+
+
+# Get the mean and variance of a gaussian mixture
+def gaussian_mixture_stats(mus, variances):
+    mean_mu = np.mean(mus)
+    mean_var = np.mean(variances) + np.mean(np.square(mus)) - np.square(mean_mu)
+    return mean_mu, mean_var
+
+
+# Create artificial regression dataset where only S out of P feature
+# dimensions contain signal.
+def get_data(N=20, S=2, P=10, sigma_obs=0.05):
+    assert S < P and P > 1 and S > 0
+    onp.random.seed(0)
+
+    X = onp.random.randn(N, P)
+    # generate S coefficients with non-negligible magnitude
+    W = 0.5 + 2.5 * onp.random.rand(S)
+    Y = onp.sum(X[:, 0:S] * W, axis=-1) + sigma_obs * onp.random.randn(N)
+    Y -= np.mean(Y)
+    Y_std = np.std(Y)
+
+    assert X.shape == (N, P)
+    assert Y.shape == (N,)
+
+    return X, Y / Y_std, W / Y_std
+
+
+# Helper function for analyzing the posterior statistics for coefficient theta_i
+def analyze_dimension(samples, X, Y, dimension, hypers):
+    vmap_args = (samples['msq_inv'], samples['lambda'], samples['eta1'],
+                 samples['xisq_inv'], samples['prec_obs'])
+    mus, variances = vmap(lambda msq_inv, lam, eta1, xisq_inv, prec_obs:
+                          compute_mean_variance(X, Y, dimension, msq_inv, lam,
+                                                eta1, xisq_inv, hypers['c'], prec_obs))(*vmap_args)
+    mean, variance = gaussian_mixture_stats(mus, variances)
+    std = np.sqrt(variance)
+    return mean, std
+
+
+def main(args):
+    jax_config.update('jax_platform_name', args.device)
+    X, Y, expected_thetas = get_data(N=args.num_data, P=args.num_dimensions, S=args.active_dimensions)
+
+    # setup hyperparameters
+    hypers = {'expected_sparsity': max(1.0, args.num_dimensions / 10),
+              'alpha1': 3.0, 'beta1': 1.0,
+              'alpha2': 3.0, 'beta2': 1.0,
+              'alpha3': 1.0, 'c': 1.0,
+              'alpha_obs': 3.0, 'beta_obs': 1.0}
+
+    # do inference
+    rng = random.PRNGKey(0)
+    samples = run_inference(model, args, rng, X, Y, hypers)
+
+    # compute the mean and square root variance of each coefficient theta_i
+    means, stds = vmap(lambda dim: analyze_dimension(samples, X, Y, dim, hypers))(np.arange(args.num_dimensions))
+
+    print("Coefficients theta_1 to theta_%d used to generate the data:" % args.active_dimensions, expected_thetas)
+    total_active_dimensions = 0
+
+    for dim, (mean, std) in enumerate(zip(means, stds)):
+        # we mark the dimension as inactive if the interval [mean - 3 * std, mean + 3 * std] contains zero
+        lower, upper = mean - 3.0 * std, mean + 3.0 * std
+        inactive = "inactive" if lower < 0.0 and upper > 0.0 else "active"
+        if inactive == "active":
+            total_active_dimensions += 1
+        print("[dimension %02d/%02d]  %s:\t%.2e +- %.2e" % (dim + 1, args.num_dimensions, inactive, mean, std))
+
+    print("Identified a total of %d active dimensions; expected %d." % (total_active_dimensions,
+                                                                        args.active_dimensions))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Gaussian Process example")
+    parser.add_argument("-n", "--num-samples", nargs="?", default=1000, type=int)
+    parser.add_argument("--num-warmup", nargs='?', default=500, type=int)
+    parser.add_argument("--num-chains", nargs='?', default=1, type=int)
+    parser.add_argument("--num-data", nargs='?', default=100, type=int)
+    parser.add_argument("--num-dimensions", nargs='?', default=20, type=int)
+    parser.add_argument("--active-dimensions", nargs='?', default=3, type=int)
+    parser.add_argument("--device", default='cpu', type=str, help='use "cpu" or "gpu".')
+    args = parser.parse_args()
+    main(args)
