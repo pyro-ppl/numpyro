2abf8f406e9f1748fb6d7cef144daec842c4c51e
Du Phan <fehiepsi@gmail.com>
2019-06-25 01:12:14 -0400

    Add delta distribution (#217)

--- examples/hmm.py
+++ examples/hmm.py
@@ -112,9 +112,8 @@ def semi_supervised_hmm(transition_prior, emission_prior,
                                 transition_log_prob, emission_log_prob)
     log_prob = logsumexp(log_prob, axis=0, keepdims=True)
     # inject log_prob to potential function
-    # NB: This is a trick (uses an invalid value `0` of Multinomial distribution) to add an
-    # additional term to potential energy.
-    return sample('forward_log_prob', dist.Multinomial(logits=-log_prob), obs=0)
+    # NB: This is a trick to add an additional term to potential energy.
+    sample('forward_log_prob', dist.Delta(log_density=log_prob), obs=0.)
 
 
 def print_results(posterior, transition_prob, emission_prob):
