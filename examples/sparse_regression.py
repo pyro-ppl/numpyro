d0af13fb71d54e84e7ced16ac2fc361b6f37e106
Martin Jankowiak <jankowiak@uber.com>
2019-07-02 09:01:14 -0700

    initial commit

--- examples/sparse_regression.py
+++ examples/sparse_regression.py
@@ -22,7 +22,7 @@ approach described in [1]. This approach is particularly suitable for situations
 with many feature dimensions (large P) but not too many datapoints (small N).
 In particular we consider a quadratic regressor of the form:
 
-f(X) = constant + theta_i X_i + theta_ij X_i X_j + observation noise
+f(X) = constant + sum_i theta_i X_i + sum_{i<j} theta_ij X_i X_j + observation noise
 
 References
 [1] The Kernel Interaction Trick: Fast Bayesian Discovery of Pairwise
@@ -37,14 +37,15 @@ def dot(X, Z):
 
 
 # The kernel that corresponds to our quadratic regressor.
-# (Note that we put the same prior on theta_ij for i=j as i!=j)
-def kernel(X, Z, eta1, eta2, c):
+def kernel(X, Z, eta1, eta2, c, jitter=1.0e-6):
     eta1sq = np.square(eta1)
     eta2sq = np.square(eta2)
     k1 = 0.5 * eta2sq * np.square(1.0 + dot(X, Z))
-    k2 = 0.5 * eta2sq * dot(np.square(X), np.square(Z))
+    k2 = -0.5 * eta2sq * dot(np.square(X), np.square(Z))
     k3 = (eta1sq - eta2sq) * dot(X, Z)
     k4 = np.square(c) - 0.5 * eta2sq
+    if X.shape == Z.shape:
+        k4 += jitter * np.eye(X.shape[0])
     return k1 + k2 + k3 + k4
 
 
@@ -107,8 +108,8 @@ def compute_singleton_mean_variance(X, Y, dimension, msq, lam, eta1, xisq, c, va
     return mu, var
 
 
-# Compute the mean and variance of coefficient theta_ij with i != j for a
-# MCMC sample (eta1, xi, ...). Compare to theorem 5.1 in reference [1].
+# Compute the mean and variance of coefficient theta_ij for a MCMC sample of the
+# kernel hyperparameters (eta1, xisq, ...). Compare to theorem 5.1 in reference [1].
 def compute_pairwise_mean_variance(X, Y, dim1, dim2, msq, lam, eta1, xisq, c, var_obs):
     P, N = X.shape[1], X.shape[0]
 
@@ -138,7 +139,64 @@ def compute_pairwise_mean_variance(X, Y, dim1, dim2, msq, lam, eta1, xisq, c, va
     return mu, var
 
 
-# Helper function for doing hmc inference
+# Sample coefficients theta from the posterior for a given MCMC sample.
+# The first P returned values are {theta_1, theta_2, ...., theta_P}, while
+# the remaining values are {theta_ij} for i,j in the list `active_dims`,
+# sorted so that i < j.
+def sample_theta_space(X, Y, active_dims, msq, lam, eta1, xisq, c, var_obs):
+    P, N, M = X.shape[1], X.shape[0], len(active_dims)
+    # the total number of coefficients we return
+    num_coefficients = P + M * (M - 1) // 2
+
+    probe = np.zeros((2 * P + 2 * M * (M - 1), P))
+    vec = np.zeros((num_coefficients, 2 * P + 2 * M * (M - 1)))
+    start1 = 0
+    start2 = 0
+
+    for dim in range(P):
+        probe = jax.ops.index_update(probe, jax.ops.index[start1:start1 + 2, dim], np.array([1.0, -1.0]))
+        vec = jax.ops.index_update(vec, jax.ops.index[start2, start1:start1 + 2], np.array([0.5, -0.5]))
+        start1 += 2
+        start2 += 1
+
+    for dim1 in active_dims:
+        for dim2 in active_dims:
+            if dim1 >= dim2:
+                continue
+            probe = jax.ops.index_update(probe, jax.ops.index[start1:start1 + 4, dim1],
+                                         np.array([1.0, 1.0, -1.0, -1.0]))
+            probe = jax.ops.index_update(probe, jax.ops.index[start1:start1 + 4, dim2],
+                                         np.array([1.0, -1.0, 1.0, -1.0]))
+            vec = jax.ops.index_update(vec, jax.ops.index[start2, start1:start1 + 4],
+                                       np.array([0.25, -0.25, -0.25, 0.25]))
+            start1 += 4
+            start2 += 1
+
+    eta2 = np.square(eta1) * np.sqrt(xisq) / msq
+    kappa = np.sqrt(msq) * lam / np.sqrt(msq + np.square(eta1 * lam))
+
+    kX = kappa * X
+    kprobe = kappa * probe
+
+    k_xx = kernel(kX, kX, eta1, eta2, c) + var_obs * np.eye(N)
+    k_xx_inv = np.linalg.inv(k_xx)
+    k_probeX = kernel(kprobe, kX, eta1, eta2, c)
+    k_prbprb = kernel(kprobe, kprobe, eta1, eta2, c)
+
+    mu = np.matmul(k_probeX, np.matmul(k_xx_inv, Y))
+    mu = np.sum(mu * vec, axis=-1)
+
+    covar = k_prbprb - np.matmul(k_probeX, np.matmul(k_xx_inv, np.transpose(k_probeX)))
+    covar = np.matmul(vec, np.matmul(covar, np.transpose(vec)))
+    L = np.linalg.cholesky(covar)
+
+    # sample from N(mu, covar)
+    sample = mu + np.matmul(L, onp.random.randn(num_coefficients))
+
+    return sample
+
+
+# Helper function for doing HMC inference
 def run_inference(model, args, rng, X, Y, hypers):
     if args.num_chains > 1:
         rng = random.split(rng, args.num_chains)
@@ -189,7 +247,7 @@ def analyze_dimension(samples, X, Y, dimension, hypers):
     return mean, std
 
 
-# Helper function for analyzing the posterior statistics for coefficient theta_ij for i!=j
+# Helper function for analyzing the posterior statistics for coefficient theta_ij
 def analyze_pair_of_dimensions(samples, X, Y, dim1, dim2, hypers):
     vmap_args = (samples['msq'], samples['lambda'], samples['eta1'], samples['xisq'], samples['var_obs'])
     mus, variances = vmap(lambda msq, lam, eta1, xisq, var_obs:
@@ -235,7 +293,7 @@ def main(args):
                                                                         args.active_dimensions))
 
     # Compute the mean and square root variance of coefficients theta_ij for i,j active dimensions.
-    # Note that the resulting numbers are only correct for i != j.
+    # Note that the resulting numbers are only meaningful for i != j.
     if len(active_dimensions) > 0:
         dim_pairs = np.array(list(itertools.product(active_dimensions, active_dimensions)))
         means, stds = vmap(lambda dim_pair: analyze_pair_of_dimensions(samples, X, Y,
@@ -248,6 +306,13 @@ def main(args):
             if not (lower < 0.0 and upper > 0.0):
                 format_str = "Identified pairwise interaction between dimensions %d and %d: %.2e +- %.2e"
                 print(format_str % (dim1 + 1, dim2 + 1, mean, std))
+
+    # Draw a single sample of coefficients theta, where we return all singleton coefficients theta_i and
+    # pairwise coefficients theta_ij for i, j active dimensions. We use the final MCMC sample obtained from
+    # the HMC sampler.
+    thetas = sample_theta_space(X, Y, active_dimensions, samples['msq'][-1], samples['lambda'][-1],
+                                samples['eta1'][-1], samples['xisq'][-1], hypers['c'], samples['var_obs'][-1])
+    print("Single posterior sample theta:\n", thetas)
 
 
 if __name__ == "__main__":
