018a23c0da51607486a60f30b8fdc62ae3852f66
Neeraj Pradhan <npradhan@uber.com>
2019-07-29 23:08:25 -0700

    Implement ravel_pytree for internal usage (#267)
    * Implement ravel_pytree for internal usage
    
    * add tests
    
    * only use custom ravel_pytree in fori_collect
    
    * minor fixes; add another test
    
    * address comments

--- test/test_util.py
+++ test/test_util.py
@@ -1,7 +1,12 @@
 import jax.numpy as np
+import pytest
+from jax import lax
+from jax.lib.xla_bridge import canonicalize_dtype
 from jax.test_util import check_eq
+from jax.tree_util import tree_multimap, tree_flatten
+from numpy.testing import assert_allclose
 
-from numpyro.util import fori_collect
+from numpyro.util import fori_collect, ravel_pytree
 
 
 def test_fori_collect():
@@ -12,3 +17,20 @@ def test_fori_collect():
     expected_tree = {'i': np.array([[0.], [2.]])}
     actual_tree = fori_collect(1, 3, f, a, transform=lambda a: {'i': a['i']})
     check_eq(actual_tree, expected_tree)
+
+
+@pytest.mark.parametrize('pytree', [
+    {'a': np.array(0.), 'b': np.array([[1., 2.], [3., 4.]])},
+    {'a': np.array(0), 'b': np.array([[1, 2], [3, 4]])},
+    {'a': np.array(0), 'b': np.array([[1., 2.], [3., 4.]])},
+    {'a': 0., 'b': np.array([[1., 2.], [3., 4.]])},
+    {'a': False, 'b': np.array([[1., 2.], [3., 4.]])},
+    [False, True, 0., np.array([[1., 2.], [3., 4.]])],
+])
+def test_ravel_pytree(pytree):
+    flat, unravel_fn = ravel_pytree(pytree)
+    unravel = unravel_fn(flat)
+    tree_flatten(tree_multimap(lambda x, y: assert_allclose(x, y), unravel, pytree))
+    assert all(tree_flatten(tree_multimap(lambda x, y:
+                                          canonicalize_dtype(lax.dtype(x)) == canonicalize_dtype(lax.dtype(y)),
+                                          unravel, pytree))[0])
