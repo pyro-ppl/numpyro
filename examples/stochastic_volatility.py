e9a04ec22b08b3dc72bb1db21033ee737653f3d2
fehiepsi <fehiepsi@gmail.com>
2019-08-26 10:37:21 -0400

    no num-chains in stochastics volatility examples

--- examples/stochastic_volatility.py
+++ examples/stochastic_volatility.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 
 import numpy as onp
 
@@ -84,9 +83,4 @@ if __name__ == "__main__":
     parser.add_argument('--device', default='cpu', type=str, help='use "cpu" or "gpu".')
     parser.add_argument('--rng', default=21, type=int, help='random number generator seed')
     args = parser.parse_args()
-
-    if args.device == 'cpu' and args.num_chains <= os.cpu_count():
-        os.environ['XLA_FLAGS'] = '--xla_force_host_platform_device_count={}'.format(
-            args.num_chains)
-
     main(args)
