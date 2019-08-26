7f9cbd112f6718cdfd03a25d7e83ac0271aa58f6
fehiepsi <fehiepsi@gmail.com>
2019-08-26 00:22:50 -0400

    jax43

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,10 @@
 import os
 
-from jax.config import config
-
+from jax.config import config; config.update('jax_platform_name', 'cpu')  # noqa: E702
 from numpyro.util import set_rng_seed
 
 
 def pytest_runtest_setup(item):
-    config.update('jax_platform_name', 'cpu')
     if 'JAX_ENABLE_x64' in os.environ:
         config.update('jax_enable_x64', True)
     set_rng_seed(0)
