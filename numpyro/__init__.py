7470b0a9089fd49817c41c5cef55684e92967b09
fehiepsi <fehiepsi@gmail.com>
2019-09-29 10:32:31 -0400

    make behaviour consistence

--- numpyro/__init__.py
+++ numpyro/__init__.py
@@ -3,6 +3,4 @@ from numpyro.primitives import module, param, plate, sample  # noqa: F401
 from numpyro.version import __version__  # noqa: F401
 import numpyro.util as util
 
-# TODO: set defaults to numpyro
-# import os
-# set_host_devices(os.cpu_count())
+util.set_platform('cpu')
