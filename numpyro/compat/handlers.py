46ff65d87fa7fdf13cb1e5c395978886d3326dea
Neeraj Pradhan <npradhan@uber.com>
2019-10-08 12:06:30 -0700

    Generic compatibility wrapper for seed handler

--- numpyro/compat/handlers.py
+++ numpyro/compat/handlers.py
@@ -1,1 +1,7 @@
 from numpyro.handlers import *  # noqa: F401, F403
+from numpyro.handlers import seed as numpyro_seed
+
+
+# Compatibility wrapper for matching arg names
+def seed(fn=None, rng_seed=None):
+    return numpyro_seed(fn=fn, rng=rng_seed)
