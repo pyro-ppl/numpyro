3ff41e31cdd67b188f9270f0ef9a7357a334d279
Neeraj Pradhan <npradhan@uber.com>
2019-10-11 12:17:24 -0700

    address comments

--- numpyro/compat/pyro.py
+++ numpyro/compat/pyro.py
@@ -5,6 +5,9 @@ from numpyro.primitives import module, param, plate, sample  # noqa: F401
 
 
 def get_param_store():
-    warnings.warn('NumPyro does not have a parameter store.', category=UnsupportedAPIWarning)
+    warnings.warn('NumPyro does not have a parameter store. '
+                  'Value of SVI parameters can be obtained via '
+                  'SVI.get_params() method.',
+                  category=UnsupportedAPIWarning)
     # Return an empty dict for compatibility
     return {}
--- test/conftest.py
+++ test/conftest.py
@@ -11,10 +11,3 @@ def pytest_runtest_setup(item):
     if 'JAX_ENABLE_x64' in os.environ:
         config.update('jax_enable_x64', True)
     set_rng_seed(0)
-
-
-def pytest_runtest_call(item):
-    try:
-        item.runtest()
-    except NotImplementedError as e:
-        pytest.xfail(str(e))
