9790801914f62191da925e18c4876101f7ad38de
Neeraj Pradhan <npradhan@uber.com>
2019-10-11 18:23:56 -0700

    Add tests from pyroapi (#396)

--- numpyro/compat/pyro.py
+++ numpyro/compat/pyro.py
@@ -1,1 +1,13 @@
+import warnings
+
+from numpyro.compat.util import UnsupportedAPIWarning
 from numpyro.primitives import module, param, plate, sample  # noqa: F401
+
+
+def get_param_store():
+    warnings.warn('NumPyro does not have a parameter store. '
+                  'Value of SVI parameters can be obtained via '
+                  'SVI.get_params() method.',
+                  category=UnsupportedAPIWarning)
+    # Return an empty dict for compatibility
+    return {}
