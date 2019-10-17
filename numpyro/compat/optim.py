a52bbea9d799cba7dbcaccc8d846d2e0b3d569bd
Neeraj Pradhan <npradhan@uber.com>
2019-10-17 13:48:16 -0700

    More compatibility tweaks for pyro-api

--- numpyro/compat/optim.py
+++ numpyro/compat/optim.py
@@ -6,3 +6,11 @@ def Adam(kwargs):
     b1, b2 = kwargs.pop('betas', (0.9, 0.999))
     eps = kwargs.pop('eps', 1.e-8)
     return optim.Adam(step_size=step_size, b1=b1, b2=b2, eps=eps)
+
+
+def ClippedAdam(kwargs):
+    step_size = kwargs.pop('lr')
+    b1, b2 = kwargs.pop('betas', (0.9, 0.999))
+    eps = kwargs.pop('eps', 1.e-8)
+    clip_norm = kwargs.pop('clip_norm', 10.)
+    return optim.ClippedAdam(step_size=step_size, b1=b1, b2=b2, eps=eps, clip_norm=clip_norm)
--- numpyro/compat/pyro.py
+++ numpyro/compat/pyro.py
@@ -1,13 +1,30 @@
 import warnings
 
 from numpyro.compat.util import UnsupportedAPIWarning
-from numpyro.primitives import module, param, plate, sample  # noqa: F401
+from numpyro.primitives import module, param as _param, plate, sample  # noqa: F401
+
+
+_PARAM_STORE = {}
 
 
 def get_param_store():
-    warnings.warn('NumPyro does not have a parameter store. '
-                  'Value of SVI parameters can be obtained via '
-                  'SVI.get_params() method.',
+    warnings.warn('A limited parameter store is provided for compatibility with Pyro. '
+                  'Value of SVI parameters should be obtained via SVI.get_params() method.',
                   category=UnsupportedAPIWarning)
     # Return an empty dict for compatibility
-    return {}
+    return _PARAM_STORE
+
+
+def clear_param_store():
+    return _PARAM_STORE.clear()
+
+
+def param(name, *args, **kwargs):
+    # Get value assuming statement is wrapped in a substitute handler.
+    val = _param(name, *args, **kwargs)
+    # If no value is found, check param store.
+    if val is None:
+        param_store = get_param_store()
+        if name in param_store:
+            val = param_store[name]
+    return val
