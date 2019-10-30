7bec03d0a67ed37b77bfacf72672b6cdfd9e5c77
Tuan Nguyen <anhtuan277@gmail.com>
2019-10-30 09:30:57 -0700

    fix lint

--- numpyro/infer/elbo.py
+++ numpyro/infer/elbo.py
@@ -74,7 +74,7 @@ class ELBO(object):
 
 class RenyiELBO(ELBO):
     r"""
-    An implementation of Renyi's :math:`\alpha`-divergence 
+    An implementation of Renyi's :math:`\alpha`-divergence
     variational inference following reference [1].
     In order for the objective to be a strict lower bound, we require
     :math:`\alpha \ge 0`. Note, however, that according to reference [1], depending
@@ -82,20 +82,19 @@ class RenyiELBO(ELBO):
     :math:`\alpha = 0`, the objective function is that of the important weighted
     autoencoder derived in reference [2].
 
-    .. Note:: Setting :math:`\alpha < 1` gives a better bound than the usual ELBO. 
-    For :math:`\alpha = 1`, it is better to use :class:`~numpyro.infer.elbo.ELBO` class because it helps reduce 
+    .. Note:: Setting :math:`\alpha < 1` gives a better bound than the usual ELBO.
+    For :math:`\alpha = 1`, it is better to use :class:`~numpyro.infer.elbo.ELBO` class because it helps reduce
     variances of gradient estimations.
 
-    :param float alpha: The order of :math:`\alpha`-divergence. 
+    :param float alpha: The order of :math:`\alpha`-divergence.
         Here :math:`\alpha \neq 1`. Default is 0.
-    :param num_particles: The number of particles/samples 
+    :param num_particles: The number of particles/samples
         used to form the objective (gradient) estimator. Default is 2.
 
     **References:**
 
     1. *Renyi Divergence Variational Inference*, Yingzhen Li, Richard E. Turner
     2. *Importance Weighted Autoencoders*, Yuri Burda, Roger Grosse, Ruslan Salakhutdinov
-    
     """
     def __init__(self, alpha=0, num_particles=2):
         if alpha == 1:
