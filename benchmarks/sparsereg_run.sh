7b4baeafe4b0c45b1ae732e2ab0829382d274a3f
Neeraj Pradhan <npradhan@uber.com>
2019-12-02 18:34:42 -0800

    update driver script

--- benchmarks/sparsereg_run.sh
+++ benchmarks/sparsereg_run.sh
@@ -2,19 +2,19 @@
 set -xe
 
 device=cpu
-N=100
+backend=numpyro  #stan
+N=200
 benchmark_dir=$( cd $(dirname "$0") ; pwd -P )
 
-for backend in numpyro stan; do
-    for seed in 1 2 3 4 5; do
-        for P in 10 20 30 40 50 60 70 80; do
-            if [[ ${device} = "cpu" ]]; then
-                python ${benchmark_dir}/sparse_regression.py --backend ${backend} --num-data ${N} \
-                    --num-dimensions ${P} --device ${device} --seed ${seed} --x64 #--disable-progbar
-            else
-                python ${benchmark_dir}/sparse_regression.py --backend ${backend} --num-data ${N} \
-                    --num-dimensions ${P} --device ${device} --seed ${seed} #--disable-progbar
-            fi
-        done
+
+for seed in 1 2 3 4 5; do
+    for P in 8 16 32 64 128 256 512; do
+        if [[ ${device} = "cpu" ]]; then
+            python ${benchmark_dir}/sparse_regression.py --backend ${backend} --num-data ${N} \
+                --num-dimensions ${P} --device ${device} --seed ${seed} --x64 --disable-progbar
+        else
+            python ${benchmark_dir}/sparse_regression.py --backend ${backend} --num-data ${N} \
+                --num-dimensions ${P} --device ${device} --seed ${seed} --disable-progbar
+        fi
     done
 done
