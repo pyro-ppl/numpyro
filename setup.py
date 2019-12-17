28ab54db2bc05c3b8bf11babecac9cf3213e169f
fehiepsi <fehiepsi@gmail.com>
2019-12-17 00:51:45 -0500

    relax jax version

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ setup(
     author_email='npradhan@uber.com',
     install_requires=[
         # TODO: pin to a specific version for the release (unless JAX's API becomes stable)
-        'jax==0.1.53',
-        'jaxlib==0.1.36',
+        'jax>=0.1.55',
+        'jaxlib>=0.1.36',
         'tqdm',
     ],
     extras_require={
