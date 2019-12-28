429d4636e3e2e8ef78dbcd41c29023e2cdab1d36
fehiepsi <fehiepsi@gmail.com>
2019-12-27 23:42:14 -0500

    add ode example

---
+++ examples/ode.py
@@ -0,0 +1,111 @@
+"""
+Predator-Prey Model
+===================
+
+This example replicates the great case study [1], which leverages the Lotka-Volterra
+equation [2] to describe the dynamics of Canada lynx (predator) and snowshoe hare
+(prey) populations. We will use the dataset obtained from [3] and run MCMC to get
+inferences about parameters of the differential equation governing the dynamics.
+
+**References:**
+
+    1. Bob Carpenter (2018), `"Predator-Prey Population Dynamics: the Lotka-Volterra model in Stan"
+       <https://mc-stan.org/users/documentation/case-studies/lotka-volterra-predator-prey.html/>`_.
+    2. https://en.wikipedia.org/wiki/Lotka–Volterra_equations
+    3. http://people.whitman.edu/~hundledr/courses/M250F03/M250.html
+"""
+
+import argparse
+import os
+
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from jax.experimental.ode import build_odeint
+import jax.numpy as np
+from jax.random import PRNGKey
+
+import numpyro
+import numpyro.distributions as dist
+from numpyro.infer import MCMC, NUTS, Predictive
+
+matplotlib.use('Agg')  # noqa: E402
+
+
+def dz_dt(z, t, alpha, beta, gamma, delta):
+    """
+    Lotka–Volterra equations. Real positive parameters `alpha`, `beta`, `gamma`, `delta`
+    describes the interaction of two species.
+    """
+    u = z[0]
+    v = z[1]
+    du_dt = (alpha - beta * v) * u
+    dv_dt = (-gamma + delta * u) * v
+    return np.stack([du_dt, dv_dt])
+
+
+ode_integrate = build_odeint(dz_dt, rtol=1e-5, atol=1e-3, mxstep=500)
+
+
+def model(N, y=None):
+    """
+    :param int N: number of measurement times
+    :param numpy.ndarray y: measured populations with shape (N, 2)
+    """
+    # initial population
+    z_init = numpyro.sample("z_init", dist.LogNormal(np.log(10), 1), sample_shape=(2,))
+    # measurement times
+    ts = np.arange(float(N))
+    # parameters alpha, beta, gamma, delta of dz_dt
+    theta = numpyro.sample(
+        "theta",
+        dist.TruncatedNormal(low=0., loc=np.array([1, 0.05, 1, 0.05]),
+                             scale=np.array([0.5, 0.05, 0.5, 0.05])))
+    # integrate dz/dt, the result will have shape N x 2
+    z = ode_integrate(z_init, ts, *theta)
+    # measurement errors
+    sigma = numpyro.sample("sigma", dist.LogNormal(-1, 1), sample_shape=(2,))
+    # measured populations (in log scale)
+    numpyro.sample("y", dist.Normal(np.log(z), sigma), obs=y)
+
+
+def main(args):
+    URL = "http://people.whitman.edu/~hundledr/courses/M250F03/LynxHare.txt"
+    data = pd.read_csv(URL, sep=" (?!$)", names=["year", "hare", "lynx"], index_col=0, engine="python")
+
+    mcmc = MCMC(NUTS(model, dense_mass=True),
+                args.num_warmup, args.num_samples, num_chains=args.num_chains,
+                progress_bar=False if "NUMPYRO_SPHINXBUILD" in os.environ else True)
+    mcmc.run(PRNGKey(1), N=data.shape[0], y=np.log(data.values))
+    mcmc.print_summary()
+
+    # predict populations
+    y_pred = Predictive(model, mcmc.get_samples())(PRNGKey(2), data.shape[0])["y"]
+    pop_pred = np.exp(y_pred)
+    plt.fill_between(data.index, data.values[:, 0], color="y", alpha=0.2, label="true hare")
+    plt.plot(data.index, data.values[:, 1], "kx", label="true lynx")
+    plt.plot(data.index, np.mean(pop_pred[..., 0], 0), "b-", label="pred hare")
+    plt.plot(data.index, np.mean(pop_pred[..., 1], 0), "g--", label="pred lynx")
+    plt.gca().set(ylim=(0, 160), xlabel="year", ylabel="population (in thousands)")
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = [3, 0, 1, 2]
+    plt.legend([handles[idx] for idx in order], [labels[idx] for idx in order])
+
+    plt.savefig("ode_plot.pdf")
+    plt.tight_layout()
+
+
+if __name__ == '__main__':
+    assert numpyro.__version__.startswith('0.2.3')
+    parser = argparse.ArgumentParser(description='Predator-Prey Model')
+    parser.add_argument('-n', '--num-samples', nargs='?', default=1000, type=int)
+    parser.add_argument('--num-warmup', nargs='?', default=1000, type=int)
+    parser.add_argument("--num-chains", nargs='?', default=1, type=int)
+    parser.add_argument('--device', default='cpu', type=str, help='use "cpu" or "gpu".')
+    args = parser.parse_args()
+
+    numpyro.set_platform(args.device)
+    numpyro.set_host_device_count(args.num_chains)
+
+    main(args)
