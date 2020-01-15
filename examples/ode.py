369299bf1df792a8a4f865c43cb3989141c87487
fehiepsi <fehiepsi@gmail.com>
2020-01-15 11:20:39 -0500

    add clearer title

--- examples/ode.py
+++ examples/ode.py
@@ -92,7 +92,7 @@ def main(args):
     plt.fill_between(year, pi[0, :, 0], pi[1, :, 0], color="k", alpha=0.2)
     plt.fill_between(year, pi[0, :, 1], pi[1, :, 1], color="b", alpha=0.3)
     plt.gca().set(ylim=(0, 160), xlabel="year", ylabel="population (in thousands)")
-    plt.title("Posterior Predictive with 80% CI.")
+    plt.title("Posterior predictive (80% CI) with predator-prey pattern.")
     plt.legend()
 
     plt.savefig("ode_plot.pdf")
