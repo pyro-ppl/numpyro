e2bbde2221ee2fab8e86f566353224be2156ef3b
fehiepsi <fehiepsi@gmail.com>
2020-01-22 23:13:32 -0500

    use random key 0 instead of 1

--- examples/ode.py
+++ examples/ode.py
@@ -78,7 +78,7 @@ def main(args):
     mcmc = MCMC(NUTS(model, dense_mass=True),
                 args.num_warmup, args.num_samples, num_chains=args.num_chains,
                 progress_bar=False if "NUMPYRO_SPHINXBUILD" in os.environ else True)
-    mcmc.run(PRNGKey(1), N=data.shape[0], y=np.log(data))
+    mcmc.run(PRNGKey(0), N=data.shape[0], y=np.log(data))
     mcmc.print_summary()
 
     # predict populations
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Predator-Prey Model')
     parser.add_argument('-n', '--num-samples', nargs='?', default=1000, type=int)
     parser.add_argument('--num-warmup', nargs='?', default=1000, type=int)
-    parser.add_argument("--num-chains", nargs='?', default=2, type=int)
+    parser.add_argument("--num-chains", nargs='?', default=1, type=int)
     parser.add_argument('--device', default='cpu', type=str, help='use "cpu" or "gpu".')
     args = parser.parse_args()
 
