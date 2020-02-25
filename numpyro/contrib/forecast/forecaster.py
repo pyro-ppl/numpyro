40384ae32af36f95ce575ed36cacde523028b358
fehiepsi <fehiepsi@gmail.com>
2020-02-24 23:55:31 -0500

    add forecast module

---
+++ numpyro/contrib/forecast/forecaster.py
@@ -0,0 +1,7 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+
+class Forecaster:
+    def __init__(self, model, data, seasonality):
+        super().__init__()
