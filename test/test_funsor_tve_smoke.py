e7b448520488518ec4d2d5c093343714d6c6e2f6
Eli <eli@elibingham.com>
2020-04-28 23:26:22 -0700

    add another highly simplified performance smoke test

--- test/test_funsor_tve_smoke.py
+++ test/test_funsor_tve_smoke.py
@@ -15,6 +15,8 @@ from funsor.tensor import Tensor
 from funsor.testing import assert_close, make_chain_einsum, make_einsum_example, make_hmm_einsum, make_plated_hmm_einsum
 from funsor.util import get_backend
 
+from numpyro.contrib.indexing import Vindex
+
 funsor.set_backend("jax")
 
 
@@ -26,13 +28,6 @@ def raw_einsum(operands, equation=None, plates=None, backend='funsor.einsum.nump
         ) for operand, inputs in zip(operands, equation.split("->")[0].split(","))
     )
     return einsum(equation, *funsor_operands, plates=plates, backend=backend).data
-
-
-OPTIMIZED_EINSUM_EXAMPLES = [
-    make_chain_einsum(t) for t in range(2, 50, 10)
-] + [
-    make_hmm_einsum(t) for t in range(2, 50, 10)
-]
 
 
 PLATED_EINSUM_EXAMPLES = [
@@ -48,10 +43,42 @@ PLATED_EINSUM_EXAMPLES = [
 ])
 def test_optimized_plated_einsum_smoke(equation, plates, backend):
 
-    jit_raw_einsum = jax.jit(functools.partial(
-        raw_einsum, equation=equation, plates=plates, backend=backend))
+    jit_raw_einsum = jax.jit(jax.value_and_grad(functools.partial(
+        raw_einsum, equation=equation, plates=plates, backend=backend)))
 
     for i in range(3):
         operands = make_einsum_example(equation)[3]
-        actual = jit_raw_einsum(operands)
+        actual, grads = jit_raw_einsum(operands)
+        assert np.ndim(actual) == 0
+
+
+def raw_hmm_with_indexing(operands, equation=None, backend='funsor.einsum.numpy_log'):
+    indexed_operands = []
+    for i, operand in enumerate(operands):
+        if np.ndim(operand) == 1:
+            y = np.arange(operand.shape[1]).reshape((operand.shape[0], 1))
+            indexed_operands.append(Vindex(operand)[y].squeeze())
+        elif np.ndim(operand) == 2:
+            x = np.arange(operand.shape[0]).reshape((operand.shape[0], 1, 1))
+            y = np.arange(operand.shape[1]).reshape((operand.shape[1], 1))
+            indexed_operands.append(Vindex(operand)[x, y].squeeze())
+        else:
+            raise ValueError
+    return raw_einsum(tuple(indexed_operands), equation=equation, plates=None, backend=backend)
+
+
+@pytest.mark.parametrize('equation', [
+    make_hmm_einsum(t) for t in range(2, 50, 10)
+])
+@pytest.mark.parametrize('backend', [
+    'funsor.einsum.numpy_log',
+])
+def test_hmm_einsum_smoke(equation, backend):
+
+    jit_raw_einsum = jax.jit(jax.value_and_grad(functools.partial(
+        raw_hmm_with_indexing, equation=equation, backend=backend)))
+
+    for i in range(3):
+        operands = make_einsum_example(equation)[3]
+        actual, grads = jit_raw_einsum(operands)
         assert np.ndim(actual) == 0
