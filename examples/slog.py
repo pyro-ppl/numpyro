1f7d83ffa7f369f7f3f4d79d6c2727d8a684164f
Martin Jankowiak <jankowiak@uber.com>
2020-05-07 11:14:22 -0700

    simplify

--- examples/slog.py
+++ examples/slog.py
@@ -17,8 +17,10 @@ import numpyro
 import numpyro.distributions as dist
 from numpyro.infer import MCMC, NUTS
 
-from jax.scipy.linalg import cho_factor, cho_solve, solve_triangular
+from jax.scipy.linalg import cho_factor, solve_triangular, cho_solve
 from numpyro.util import enable_x64
+
+import pickle
 
 
 def sigmoid(x):
@@ -27,6 +29,12 @@ def sigmoid(x):
 
 def dot(X, Z):
     return np.dot(X, Z[..., None])[..., 0]
+
+
+def cho_tri_solve(A, b):
+    L = cho_factor(A, lower=True)[0]
+    Linv_b = solve_triangular(L, b, lower=True)
+    return L, Linv_b
 
 
 # The kernel that corresponds to our quadratic logit function
@@ -46,7 +54,8 @@ def kernel(X, Z, eta1, eta2, c, jitter=1.0e-6):
 def model(X, Y, hypers):
     S, P, N = hypers['expected_sparsity'], X.shape[1], X.shape[0]
 
-    sigma = numpyro.sample("sigma", dist.HalfNormal(hypers['alpha3']))
+    #sigma = numpyro.sample("sigma", dist.HalfNormal(hypers['alpha3']))
+    sigma = 4.0
     phi = sigma * (S / np.sqrt(N)) / (P - S)
     eta1 = numpyro.sample("eta1", dist.HalfCauchy(phi))
 
@@ -64,11 +73,11 @@ def model(X, Y, hypers):
     k = kernel(kX, kX, eta1, eta2, hypers['c'])
 
     k_omega = k + np.eye(N) * (1.0 / omega)
-    L = cho_factor(k_omega, lower=True)[0]
 
     kY = np.matmul(k, Y)
+    L, Linv_kY = cho_tri_solve(k_omega, kY)
+
     log_factor1 = dot(Y, kY)
-    Linv_kY = solve_triangular(L, kY, lower=True)
     log_factor2 = dot(Linv_kY, Linv_kY)
     log_factor3 = np.sum(np.log(np.diagonal(L))) + 0.5 * np.sum(np.log(omega))
 
@@ -76,64 +85,38 @@ def model(X, Y, hypers):
     numpyro.factor("obs", obs_factor)
 
 
-# Compute the mean and variance of coefficient theta_i (where i = dimension) for a
-# MCMC sample of the kernel hyperparameters (eta1, eta2, ...).
-def compute_singleton_mean_variance(X, Y, dimension, eta1, eta2, c, kappa, omega):
-    P, N = X.shape[1], X.shape[0]
-
-    probe = np.zeros((2, P))
-    probe = jax.ops.index_update(probe, jax.ops.index[:, dimension], np.array([1.0, -1.0]))
-
+def compute_coefficient_mean_variance(X, Y, probe, vec, eta1, eta2, c, kappa, omega):
     kprobe, kX = kappa * probe, kappa * X
 
     k_xx = kernel(kX, kX, eta1, eta2, c)
-
     k_probeX = kernel(kprobe, kX, eta1, eta2, c)
     k_prbprb = kernel(kprobe, kprobe, eta1, eta2, c)
 
-    M = np.linalg.inv(np.eye(N) + omega[:, None] * k_xx)
-    mu = 0.5 * np.matmul(M, Y)
-    mu = dot(k_probeX, mu)
+    L = cho_factor(k_xx + np.eye(X.shape[0]) * (1.0 / omega), lower=True)[0]
 
-    vec = np.array([0.50, -0.50])
-    mu = np.dot(mu, vec)
+    mu = 0.5 * cho_solve((L, True), Y / omega)
+    mu = np.dot(vec, dot(k_probeX, mu))
 
-    k_omega_inv = np.linalg.inv(k_xx + np.eye(N) * (1.0 / omega))
-    var = k_prbprb - np.matmul(k_probeX, np.matmul(k_omega_inv, np.transpose(k_probeX)))
-    var = np.matmul(var, vec)
-    var = np.dot(var, vec)
+    Linv_kXprobe = solve_triangular(L, np.transpose(k_probeX), lower=True)
+    var = k_prbprb - np.matmul(np.transpose(Linv_kXprobe), Linv_kXprobe)
+    var = np.dot(vec, np.matmul(var, vec))
 
     return mu, var
 
 
-# Compute the mean and variance of coefficient theta_ij for a MCMC sample of the
-# kernel hyperparameters (eta1, eta2, ...).
-def compute_pairwise_mean_variance(X, Y, dim1, dim2, eta1, eta2, c, kappa, omega):
-    P, N = X.shape[1], X.shape[0]
+def compute_singleton_mean_variance(X, Y, dimension, eta1, eta2, c, kappa, omega):
+    probe = np.zeros((2, X.shape[1]))
+    probe = jax.ops.index_update(probe, jax.ops.index[:, dimension], np.array([1.0, -1.0]))
+    vec = np.array([0.50, -0.50])
+    return compute_coefficient_mean_variance(X, Y, probe, vec, eta1, eta2, c, kappa, omega)
 
-    probe = np.zeros((4, P))
+
+def compute_pairwise_mean_variance(X, Y, dim1, dim2, eta1, eta2, c, kappa, omega):
+    probe = np.zeros((4, X.shape[1]))
     probe = jax.ops.index_update(probe, jax.ops.index[:, dim1], np.array([1.0, 1.0, -1.0, -1.0]))
     probe = jax.ops.index_update(probe, jax.ops.index[:, dim2], np.array([1.0, -1.0, 1.0, -1.0]))
-
-    kprobe, kX = kappa * probe, kappa * X
-
-    k_xx = kernel(kX, kX, eta1, eta2, c)
-    k_probeX = kernel(kprobe, kX, eta1, eta2, c)
-    k_prbprb = kernel(kprobe, kprobe, eta1, eta2, c)
-
-    M = np.linalg.inv(np.eye(N) + omega[:, None] * k_xx)
-    mu = 0.5 * np.matmul(M, Y)
-    mu = dot(k_probeX, mu)
-
     vec = np.array([0.25, -0.25, -0.25, 0.25])
-    mu = np.dot(mu, vec)
-
-    k_omega_inv = np.linalg.inv(k_xx + np.eye(N) * (1.0 / omega))
-    var = k_prbprb - np.matmul(k_probeX, np.matmul(k_omega_inv, np.transpose(k_probeX)))
-    var = np.matmul(var, vec)
-    var = np.dot(var, vec)
-
-    return mu, var
+    return compute_coefficient_mean_variance(X, Y, probe, vec, eta1, eta2, c, kappa, omega)
 
 
 # Helper function for doing HMC inference
@@ -167,13 +150,13 @@ def get_data(N=20, S=2, P=10):
     X *= flip
 
     # generate S coefficients with non-negligible magnitude
-    W = 1.5 + 2.5 * onp.random.rand(S)
+    W = 2.0 + 3.0 * onp.random.rand(S)
     flip = 2 * onp.random.binomial(1, 0.5, W.shape) - 1
     W *= flip
 
     # generate data using the S coefficients and a single pairwise interaction
     pairwise_coefficient = 3.0
-    Y = onp.sum(X[:, 0:S] * W, axis=-1) + pairwise_coefficient * X[:, 0] * X[:, 1]
+    Y = onp.sum(X[:, 0:S] * W, axis=-1) + pairwise_coefficient * (X[:, 0] * X[:, 1] - X[:, 2] * X[:, 3])
     Y = 2 * onp.random.binomial(1, sigmoid(Y)) - 1
     print("number of 1s: {}  number of -1s: {}".format(np.sum(Y == 1.0), np.sum(Y == -1.0)))
 
@@ -204,66 +187,69 @@ def analyze_pair_of_dimensions(samples, X, Y, dim1, dim2, hypers):
 
 
 def main(args):
-    X, Y, expected_thetas, expected_pairwise = get_data(N=args.num_data, P=args.num_dimensions,
-                                                        S=args.active_dimensions)
 
-    # setup hyperparameters
-    hypers = {'expected_sparsity': max(1.0, args.num_dimensions / 10),
-              'alpha1': 3.0, 'beta1': 1.0,
-              'alpha2': 3.0, 'beta2': 1.0,
-              'alpha3': 1.0, 'c': 1.0}
+    for P in [10]:
 
-    # do inference
-    rng_key = random.PRNGKey(0)
-    samples = run_inference(model, args, rng_key, X, Y, hypers)
+        X, Y, expected_thetas, expected_pairwise = get_data(N=args.num_data, P=args.num_dimensions,
+                                                            S=args.active_dimensions)
 
-    # compute the mean and square root variance of each coefficient theta_i
-    means, stds = vmap(lambda dim: analyze_dimension(samples, X, Y, dim, hypers))(np.arange(args.num_dimensions))
+        # setup hyperparameters
+        hypers = {'expected_sparsity': max(1.0, args.num_dimensions / 10.0),
+                  'alpha1': 3.0, 'beta1': 1.0,
+                  'alpha2': 3.0, 'beta2': 1.0,
+                  'alpha3': 1.0, 'c': 1.0}
 
-    print("Coefficients theta_1 to theta_%d used to generate the data:" % args.active_dimensions, expected_thetas)
-    active_dimensions = []
+        # do inference
+        rng_key = random.PRNGKey(0)
+        samples = run_inference(model, args, rng_key, X, Y, hypers)
 
-    for dim, (mean, std) in enumerate(zip(means, stds)):
-        # we mark the dimension as inactive if the interval [mean - 2 * std, mean + 2 * std] contains zero
-        lower, upper = mean - 2.0 * std, mean + 2.0 * std
-        inactive = "inactive" if lower < 0.0 and upper > 0.0 else "active"
-        if inactive == "active":
-            active_dimensions.append(dim)
-        print("[dimension %02d/%02d]  %s:\t%.2e +- %.2e" % (dim + 1, args.num_dimensions, inactive, mean, std))
+        # compute the mean and square root variance of each coefficient theta_i
+        means, stds = vmap(lambda dim: analyze_dimension(samples, X, Y, dim, hypers))(np.arange(args.num_dimensions))
 
-    print("Identified a total of %d active dimensions; expected %d." % (len(active_dimensions),
-                                                                        args.active_dimensions))
-    print("The single quadratic coefficient theta_{1,2} used to generate the data:", expected_pairwise)
+        print("Coefficients theta_1 to theta_%d used to generate the data:" % args.active_dimensions, expected_thetas)
+        active_dimensions = []
 
-    # Compute the mean and square root variance of coefficients theta_ij for i,j active dimensions.
-    # Note that the resulting numbers are only meaningful for i != j.
-    if len(active_dimensions) > 0:
-        dim_pairs = np.array(list(itertools.product(active_dimensions, active_dimensions)))
-        means, stds = vmap(lambda dim_pair: analyze_pair_of_dimensions(samples, X, Y,
-                                                                       dim_pair[0], dim_pair[1], hypers))(dim_pairs)
-        for dim_pair, mean, std in zip(dim_pairs, means, stds):
-            dim1, dim2 = dim_pair
-            if dim1 >= dim2:
-                continue
+        for dim, (mean, std) in enumerate(zip(means, stds)):
+            # we mark the dimension as inactive if the interval [mean - 2 * std, mean + 2 * std] contains zero
             lower, upper = mean - 2.0 * std, mean + 2.0 * std
-            if not (lower < 0.0 and upper > 0.0):
-                format_str = "Identified pairwise interaction between dimensions %d and %d: %.2e +- %.2e"
-                print(format_str % (dim1 + 1, dim2 + 1, mean, std))
-            else:
-                format_str = "No pairwise interaction between dimensions %d and %d: %.2e +- %.2e"
-                print(format_str % (dim1 + 1, dim2 + 1, mean, std))
+            inactive = "inactive" if lower < 0.0 and upper > 0.0 else "active"
+            if inactive == "active":
+                active_dimensions.append(dim)
+            print("[dimension %02d/%02d]  %s:\t%.2e +- %.2e" % (dim + 1, args.num_dimensions, inactive, mean, std))
+
+        print("Identified a total of %d active dimensions; expected %d." % (len(active_dimensions),
+                                                                            args.active_dimensions))
+        print("The single quadratic coefficient theta_{1,2} used to generate the data:", expected_pairwise)
+
+        # Compute the mean and square root variance of coefficients theta_ij for i,j active dimensions.
+        # Note that the resulting numbers are only meaningful for i != j.
+        if len(active_dimensions) > 0:
+            dim_pairs = np.array(list(itertools.product(active_dimensions, active_dimensions)))
+            means, stds = vmap(lambda dim_pair: analyze_pair_of_dimensions(samples, X, Y,
+                                                                           dim_pair[0], dim_pair[1], hypers))(dim_pairs)
+            for dim_pair, mean, std in zip(dim_pairs, means, stds):
+                dim1, dim2 = dim_pair
+                if dim1 >= dim2:
+                    continue
+                lower, upper = mean - 2.0 * std, mean + 2.0 * std
+                if not (lower < 0.0 and upper > 0.0):
+                    format_str = "Identified pairwise interaction between dimensions %d and %d: %.2e +- %.2e"
+                    print(format_str % (dim1 + 1, dim2 + 1, mean, std))
+                else:
+                    format_str = "No pairwise interaction between dimensions %d and %d: %.2e +- %.2e"
+                    print(format_str % (dim1 + 1, dim2 + 1, mean, std))
 
 
 if __name__ == "__main__":
     assert numpyro.__version__.startswith('0.2.4')
     parser = argparse.ArgumentParser(description="Gaussian Process example")
-    parser.add_argument("-n", "--num-samples", nargs="?", default=700, type=int)
-    parser.add_argument("--num-warmup", nargs='?', default=200, type=int)
+    parser.add_argument("-n", "--num-samples", nargs="?", default=300, type=int)
+    parser.add_argument("--num-warmup", nargs='?', default=100, type=int)
     parser.add_argument("--num-chains", nargs='?', default=1, type=int)
-    parser.add_argument("--mtd", nargs='?', default=8, type=int)
-    parser.add_argument("--num-data", nargs='?', default=350, type=int)
-    parser.add_argument("--num-dimensions", nargs='?', default=50, type=int)
-    parser.add_argument("--active-dimensions", nargs='?', default=5, type=int)
+    parser.add_argument("--mtd", nargs='?', default=6, type=int)
+    parser.add_argument("--num-data", nargs='?', default=180, type=int)
+    parser.add_argument("--num-dimensions", nargs='?', default=40, type=int)
+    parser.add_argument("--active-dimensions", nargs='?', default=4, type=int)
     parser.add_argument("--device", default='cpu', type=str, help='use "cpu" or "gpu".')
     args = parser.parse_args()
 
