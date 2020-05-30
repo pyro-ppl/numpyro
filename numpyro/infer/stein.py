24d6da38843c90f04e4b66c0b056da0ef9cb0b13
OlaRonning <ronning@pm.me>
2020-05-30 22:09:37 +0200

    FIXED: removed debug print

--- numpyro/infer/stein.py
+++ numpyro/infer/stein.py
@@ -307,8 +307,6 @@ class SVGD:
         ts = datetime.utcnow().strftime(SVGD.STRFTIME)
         (ch_dir / ts).mkdir()
 
-        print(self.get_params(state))
-
         with bz2.open(ch_dir / ts / SVGD.STATE_FILE, 'w') as f:
             c_pickle.dump(self.get_params(state), f)
         with bz2.open(ch_dir / ts / SVGD.TRANSFORMS_FILE, 'w') as f:
