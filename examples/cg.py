fb40c460ad97fc328dcbda6317bb2da3b83072e9
Martin Jankowiak <jankowiak@uber.com>
2020-06-04 07:44:23 -0700

    lanczoswip

--- examples/cg.py
+++ examples/cg.py
@@ -9,10 +9,11 @@ import time
 from jax.util import partial
 
 
-onp.random.seed(0)
-D = 4
+#onp.random.seed(0)
+D = 6
 A = onp.random.rand(D * D // 2).reshape((D, D // 2))
 A = onp.matmul(A, onp.transpose(A)) + 0.05 * onp.eye(D)
+#A = A + onp.diag(np.array([0.1,0.2,0.3,0.4]))
 b = onp.random.rand(D)
 L = onp.linalg.cholesky(A)
 truth = cho_solve((L, True), b)
@@ -21,52 +22,52 @@ truth = cho_solve((L, True), b)
 
 
 
-CGState = namedtuple('CGState', ['u', 'r', 'd', 'rnorm', 'iter', 'alpha', 'beta'])
+CGState = namedtuple('CGState', ['u', 'r', 'd', 'r_dot_r', 'iter', 'alpha', 'beta'])
 
 def cg_body_fun(state, mvm):
-    u, r, d, rnorm, iteration, alpha, beta = state
-    v = mvm(d[..., None])[..., 0]
-    _alpha = np.dot(r, r) / np.dot(d, v)
+    u, r, d, r_dot_r, iteration, alpha, beta = state
+    v = mvm(d)
+    _alpha = r_dot_r / np.dot(d, v)
     u = u + _alpha * d
-    beta_denom = np.dot(r, r)
     r = r - _alpha * v
-    rnorm = np.linalg.norm(r)
-    _beta = np.dot(r, r) / beta_denom
+    _beta_denom = r_dot_r
+    r_dot_r = np.dot(r, r)
+    _beta = r_dot_r / _beta_denom
     d = r + _beta * d
-    alpha = jax.ops.index_update(alpha, jax.ops.index[iteration], _alpha)
-    beta = jax.ops.index_update(beta, jax.ops.index[iteration], _beta)
-    return CGState(u, r, d, rnorm, iteration + 1, alpha, beta)
+    alpha = jax.ops.index_update(alpha, iteration, _alpha)
+    beta = jax.ops.index_update(beta, iteration, _beta)
+    return CGState(u, r, d, r_dot_r, iteration + 1, alpha, beta)
 
 def cg_cond_fun(state, mvm, epsilon=1.0e-14, max_iters=100):
-    return (state.rnorm > epsilon) & (state.iter < max_iters)
+    return (np.sqrt(state.r_dot_r) > epsilon) & (state.iter < max_iters)
 
 def cg(b, A, epsilon=1.0e-14, max_iters=100):
     mvm = lambda rhs: np.matmul(A, rhs)
     _cond_fun = lambda state: cg_cond_fun(state, mvm=mvm, epsilon=epsilon, max_iters=max_iters)
     _body_fun = lambda state: cg_body_fun(state, mvm=mvm)
     zero = np.zeros(b.shape[-1])
-    init_state = CGState(zero, b, b, 1.0e10, 0, zero, zero)
+    init_state = CGState(zero, b, b, np.dot(b, b), 0, zero, zero)
     final_state = while_loop(_cond_fun, _body_fun, init_state)
-    alpha = final_state.alpha
-    beta = final_state.beta
-    P = final_state.iter
+    P, alpha, beta = final_state.iter, final_state.alpha, final_state.beta
     print("alpha", alpha)
     print("beta", beta)
-    diag = 1.0 / alpha[:P]
-    diag = jax.ops.index_add(diag, np.arange(1, P), beta[:P-1] / alpha[:P-1])
+    diag = jax.ops.index_add(1.0 / alpha[:P], np.arange(1, P), beta[:P-1] / alpha[:P-1])
     T = onp.zeros(P * P)
     offdiag = np.sqrt(beta[:P-1]) / alpha[:P-1]
     T = jax.ops.index_add(T, np.arange(1, P * P, P + 1), offdiag)
     T = jax.ops.index_add(T, np.arange(P, P * P, P + 1), offdiag)
     T = jax.ops.index_add(T, np.arange(0, P * P, P + 1), diag)
     T = T.reshape((P, P))
+    print("Teig", np.linalg.eigh(T)[0])
     print("T\n",T, onp.linalg.slogdet(T))
     print("diag0", 1/ alpha[0])
     print("diag1", 1/ alpha[1] + beta[0] / alpha[0])
     print("diag2", 1/ alpha[2] + beta[1] / alpha[1])
+    print("diag3", 1/ alpha[3] + beta[2] / alpha[2])
     print("offdiag0", np.sqrt(beta[0]) / alpha[0])
     print("offdiag1", np.sqrt(beta[1]) / alpha[1])
-    return final_state.u, final_state.rnorm, final_state.iter
+    print("offdiag2", np.sqrt(beta[2]) / alpha[2])
+    return final_state.u, np.sqrt(final_state.r_dot_r), final_state.iter
 
 #@jit
 def batch_cg(b, A, epsilon=1.0e-14, max_iters=100):
@@ -85,8 +86,10 @@ if 0:
     print("num_iters", num_iters)
 else:
     t0 = time.time()
+    print("A",A)
+    print("A symeig", np.linalg.eigh(A)[0])
     print("Aslogdet", onp.linalg.slogdet(A))
-    u, rnorm, num_iters = cg(b, A, epsilon=1.0e-14, max_iters=4)
+    u, rnorm, num_iters = cg(b, A, epsilon=1.0e-18, max_iters=D)
     t1 = time.time()
     print("took {:.5f} seconds".format(t1-t0))
 
