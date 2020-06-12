2e58f7363eef09af7166498b60846dd951f8aaaf
OlaRonning <ronning@pm.me>
2020-06-12 23:11:53 +0200

    CHANGED: np to jnp in directional.

--- numpyro/distributions/directional.py
+++ numpyro/distributions/directional.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 import math
 
-import jax.numpy as np
+import jax.numpy as jnp
 from jax import lax, dtypes
 
 from numpyro.distributions import constraints
@@ -23,7 +23,7 @@ class VonMises(Distribution):
     def __init__(self, loc, concentration, validate_args=None):
         self.loc, self.concentration = promote_shapes(loc, concentration)
 
-        batch_shape = lax.broadcast_shapes(np.shape(concentration), np.shape(loc))
+        batch_shape = lax.broadcast_shapes(jnp.shape(concentration), jnp.shape(loc))
 
         super(VonMises, self).__init__(batch_shape=batch_shape,
                                        validate_args=validate_args)
@@ -36,21 +36,21 @@ class VonMises(Distribution):
             :return: samples from von Mises
         """
         samples = von_mises_centered(key, self.concentration, shape=sample_shape,
-                                      dtype=dtypes.canonicalize_dtype(np.float64))
+                                     dtype=dtypes.canonicalize_dtype(jnp.float64))
         samples = samples + self.loc  # VM(0, concentration) -> VM(loc,concentration)
-        samples = (samples + np.pi) % (2. * np.pi) - np.pi
+        samples = (samples + jnp.pi) % (2. * jnp.pi) - jnp.pi
 
         return samples
 
     @validate_sample
     def log_prob(self, value):
-        return -(np.log(2 * np.pi) + lax.bessel_i0e(self.concentration)) + (
-                self.concentration * np.cos(value - self.loc))
+        return -(jnp.log(2 * jnp.pi) + lax.bessel_i0e(self.concentration)) + (
+                self.concentration * jnp.cos(value - self.loc))
 
     @property
     def mean(self):
         """ Computes circular mean of distribution """
-        return np.broadcast_to(self.loc, self.batch_shape)
+        return jnp.broadcast_to(self.loc, self.batch_shape)
 
     @property
     def location(self):
@@ -59,5 +59,5 @@ class VonMises(Distribution):
     @property
     def variance(self):
         """ Computes circular variance of distribution """
-        return np.broadcast_to(1 - lax.bessel_i1e(self.concentration) / lax.bessel_i0e(self.concentration),
-                               self.batch_shape)
+        return jnp.broadcast_to(1 - lax.bessel_i1e(self.concentration) / lax.bessel_i0e(self.concentration),
+                                self.batch_shape)
