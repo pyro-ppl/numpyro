e6499afe5b3165a34d52a05b67ce9da2a6da2a39
fehiepsi <fehiepsi@gmail.com>
2020-06-12 13:50:44 -0400

    add missing files

---
+++ numpyro/contrib/enum/infer_util.py
@@ -0,0 +1,37 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+import funsor
+
+from numpyro.contrib.enum.enum_messenger import packed_trace
+from numpyro.distributions.util import is_identically_one
+
+
+def enum_log_density(model, model_args, model_kwargs, params):
+    model_trace = packed_trace(model).get_trace(*model_args, **model_kwargs)
+    log_factors = []
+    sum_vars, prod_vars = frozenset(), frozenset()
+    for site in model_trace.values():
+        if site['type'] == 'sample':
+            value = site['value']
+            intermediates = site['intermediates']
+            scale = site['scale']
+            if intermediates:
+                log_prob = site['fn'].log_prob(intermediates)
+            else:
+                log_prob = site['fn'].log_prob(value)
+
+            if (scale is not None) and (not is_identically_one(scale)):
+                log_prob = scale * log_prob
+
+            log_prob = funsor.to_funsor(log_prob, output=funsor.reals(), dim_to_name=site['infer']['dim_to_name'])
+            log_factors.append(log_prob)
+            sum_vars |= frozenset({site['name']})
+            prod_vars |= frozenset(f.name for f in site['cond_indep_stack'] if f.dim is not None)
+
+    with funsor.interpreter.interpretation(funsor.terms.lazy):
+        lazy_result = funsor.sum_product.sum_product(
+            funsor.ops.logaddexp, funsor.ops.add, log_factors,
+            eliminate=sum_vars | prod_vars, plates=prod_vars)
+    result = funsor.optimizer.apply_optimizer(lazy_result)
+    return -result.data
