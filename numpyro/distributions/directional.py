UPDATED: for loop to while; CHANGED: to tests to check correct sample stats (circular mean and variance); loc domain to reals.

numpyro/distributions/directional.py
@@ -14,14 +14,19 @@ from numpyro.distributions.util import promote_shapes, von_mises_centered
 
 @copy_docs_from(Distribution)
 class VonMises(Distribution):
-    arg_constraints = {'loc': constraints.interval(-math.pi, math.pi), 'concentration': constraints.positive}
+    arg_constraints = {'loc': constraints.real, 'concentration': constraints.positive}
 
-    @property
-    def support(self):
-        return constraints.interval(-math.pi, math.pi)
+    support = constraints.interval(-math.pi, math.pi)
 
     def __init__(self, loc, concentration, validate_args=None):
-        self.loc, self.concentration = promote_shapes(loc, concentration)
+        """  von Mises distribution for sampling directions.
+
+        :param loc: center or distribution
+                    NOTE: mapped to [-pi, pi]
+        :param concentration: concentration of distribution
+        """
+        # Map loc to [-pi, pi]
+        self.loc, self.concentration = promote_shapes((loc + jnp.pi) % (2.*jnp.pi) - jnp.pi, concentration)
 
         batch_shape = lax.broadcast_shapes(jnp.shape(concentration), jnp.shape(loc))
 
@@ -32,7 +37,7 @@ class VonMises(Distribution):
         """ Generate sample from von Mises distribution
 
             :param sample_shape: shape of samples
-            :param rng_key: random number generator key
+            :param key: random number generator key
             :return: samples from von Mises
         """
         samples = von_mises_centered(key, self.concentration, sample_shape, dtypes.canonicalize_dtype(jnp.float64))

numpyro/distributions/util.py
@@ -347,27 +347,34 @@ def clamp_probs(probs):
 
 
 def von_mises_centered(key, concentration, shape=(), dtype=jnp.float64):
-    return _von_mises_centered(key, concentration, shape, dtype)
-
-
-@partial(jit, static_argnums=(2, 3))
-def _von_mises_centered(key, concentration, shape, dtype):
     """ Compute centered von Mises samples using rejection sampling from [1] with wrapped Cauchy proposal.
 
         *** References ***
         [1] Luc Devroye "Non-Uniform Random Variate Generation", Springer-Verlag, 1986;
             Chapter 9, p. 473-476. http://www.nrbook.com/devroye/Devroye_files/chapter_nine.pdf
 
-        :param sample_shape: shape of samples
-        :param rng_key: random number generator key
+
+        :param key: random number generator key
+        :param concentration: concentration of distribution
+        :param shape: shape of samples
+        :param dtype: float precesions for choosing correct s cutfoff
         :return: centered samples from von Mises
     """
+    shape = shape or jnp.shape(concentration)
+    dtype = canonicalize_dtype(dtype)
+    concentration = lax.convert_element_type(concentration, dtype)
+    concentration = jnp.broadcast_to(concentration, shape)
+    return _von_mises_centered(key, concentration, shape, dtype)
 
+
+@partial(jit, static_argnums=(2, 3))
+def _von_mises_centered(key, concentration, shape, dtype, max_iter=100):
     # Cutoff from TensorFlow probability
     # (https://github.com/tensorflow/probability/blob/f051e03dd3cc847d31061803c2b31c564562a993/tensorflow_probability/python/distributions/von_mises.py#L567-L570)
     s_cutoff_map = {jnp.dtype(jnp.float16): 1.8e-1,
                     jnp.dtype(jnp.float32): 2e-2,
                     jnp.dtype(jnp.float64): 1.2e-4}
+    s_cutoff = s_cutoff_map.get(dtype)
 
     r = 1. + jnp.sqrt(1. + 4. * concentration ** 2)
     rho = (r - jnp.sqrt(2. * r)) / (2. * concentration)
@@ -375,13 +382,15 @@ def _von_mises_centered(key, concentration, shape, dtype):
 
     s_approximate = 1. / concentration
 
-    s_cutoff = s_cutoff_map.get(dtype)
-
     s = jnp.where(concentration > s_cutoff, s_exact, s_approximate)
 
-    def body_function(i, *args):
-        done, _, w = args[0]
-        nonlocal key
+    def cond_fn(*args):
+        """ check if all are done or reached max number of iterations """
+        i, _, done, _, _ = args[0]
+        return jnp.bitwise_and(i < max_iter, jnp.logical_not(jnp.all(done)))
+
+    def body_fn(*args):
+        i, key, done, _, w = args[0]
         uni_ukey, uni_vkey, key = random.split(key, 3)
 
         u = random.uniform(key=uni_ukey, shape=shape, dtype=concentration.dtype, minval=-1., maxval=1.)
@@ -393,17 +402,16 @@ def _von_mises_centered(key, concentration, shape, dtype):
 
         accept = (y * (2. - y) >= v) | (jnp.log(y / v) + 1. >= y)
 
-        return accept | done, u, w
+        return i+1, key, accept | done, u, w
 
     init_done = jnp.zeros(shape, dtype=bool)
     init_u = jnp.zeros(shape)
     init_w = jnp.zeros(shape)
 
-    done, u, w = lax.fori_loop(
-        lower=0,
-        upper=100,
-        body_fun=body_function,
-        init_val=(init_done, init_u, init_w)
+    _, _, done, u, w = lax.while_loop(
+        cond_fun=cond_fn,
+        body_fun=body_fn,
+        init_val=(jnp.array(0), key, init_done, init_u, init_w)
     )
 
     return jnp.sign(u) * jnp.arccos(w)

test/test_distributions.py
@@ -77,7 +77,7 @@ _DIST_MAP = {
     dist.Cauchy: lambda loc, scale: osp.cauchy(loc=loc, scale=scale),
     dist.Chi2: lambda df: osp.chi2(df),
     dist.Dirichlet: lambda conc: osp.dirichlet(conc),
-    dist.Exponential: lambda rate: osp.expon(scale=np.reciprocal(rate)),
+    dist.Exponential: lambda rate: osp.expon(scale=jnp.reciprocal(rate)),
     dist.Gamma: lambda conc, rate: osp.gamma(conc, scale=1. / rate),
     dist.Gumbel: lambda loc, scale: osp.gumbel_r(loc=loc, scale=scale),
     dist.HalfCauchy: lambda scale: osp.halfcauchy(scale=scale),
@@ -653,13 +653,12 @@ def test_mean_var(jax_dist, sp_dist, params):
         assert_allclose(jnp.mean(corr_samples, axis=0), expected_mean, atol=0.01)
         assert_allclose(jnp.std(corr_samples, axis=0), expected_std, atol=0.01)
     elif jax_dist in [dist.VonMises]:
-        loc, conc = params
-        if np.all(np.isfinite(d_jax.mean)):
-            assert_allclose(loc, d_jax.mean, rtol=0.05, atol=1e-2)
-
-        # von Mises circular variance
-        expected_variance = 1 - lax.bessel_i1e(conc) / lax.bessel_i0e(conc)
-        assert_allclose(expected_variance, d_jax.variance, rtol=0.05, atol=1e-2)
+        # circular mean = sample mean
+        assert_allclose(d_jax.mean, jnp.mean(samples), rtol=0.05, atol=1e-2)
+        # circular variance
+        x, y = jnp.mean(jnp.cos(samples)), jnp.mean(jnp.sin(samples))
+        expected_variance = 1 - jnp.sqrt(x ** 2 + y ** 2)
+        assert_allclose(d_jax.variance, expected_variance, rtol=0.05, atol=1e-2)
     else:
         if jnp.all(jnp.isfinite(d_jax.mean)):
             assert_allclose(jnp.mean(samples, 0), d_jax.mean, rtol=0.05, atol=1e-2)