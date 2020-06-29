badd70bbe0016ae9809d27c61b9054bf69bfd37c
Ahmad Salim Al-Sibahi <ahmad@di.ku.dk>
2020-06-29 19:57:59 +0200

    Add more callbacks

--- numpyro/callbacks/__init__.py
+++ numpyro/callbacks/__init__.py
@@ -1,7 +1,13 @@
 from numpyro.callbacks.callback import Callback
 from numpyro.callbacks.progbar import Progbar
+from numpyro.callbacks.history import History
+from numpyro.callbacks.early_stopping import EarlyStopping
+from numpyro.callbacks.terminate_on_nan import TerminateOnNaN
 
 __all__ = [
     'Callback',
-    'Progbar'
+    'Progbar',
+    'History',
+    'EarlyStopping',
+    'TerminateOnNaN'
 ]
---
+++ numpyro/callbacks/early_stopping.py
@@ -0,0 +1,57 @@
+from numpyro.callbacks.callback import Callback
+
+
+class EarlyStopping(Callback):
+    def __init__(self, patience=10, min_delta=0.0, smoothing='dexp',
+                 data_smoothing_factor=0.6, trend_smoothing_factor=0.6,
+                 mode='training'):
+        super().__init__()
+        assert smoothing in {'none', 'exp', 'dexp'}
+        assert mode in {'training', 'validation'}
+        self.patience = patience
+        self.smoothing = smoothing
+        self.min_delta = min_delta
+        self.waiting = 0
+        self.best_loss = float('inf')
+        self.prev_loss = None
+        self.curr_loss = None
+        self.trend = None
+        self.data_smoothing_factor = data_smoothing_factor
+        self.trend_smoothing_factor = trend_smoothing_factor
+        self.mode = mode
+
+    def on_train_begin(self, train_info):
+        if self.mode == 'training':
+            self.best_loss = train_info['loss']
+            self.curr_loss = train_info['loss']
+
+    def on_train_step_end(self, step, train_info):
+        if self.mode == 'training':
+            self.update_and_early_stop(train_info['loss'])
+
+    def on_validation_end(self, val_step, val_info):
+        if self.mode == 'validation':
+            self.update_and_early_stop(val_info['loss'])
+
+    def update_and_early_stop(self, loss):
+        if self.smoothing == 'dexp':
+            self.prev_loss = self.curr_loss
+            self.curr_loss = (self.data_smoothing_factor * loss +
+                              (1 - self.data_smoothing_factor) * (self.curr_loss + self.trend))
+            if self.trend is None:
+                self.trend = loss - self.curr_loss
+            else:
+                self.trend = (self.trend_smoothing_factor * (self.curr_loss - self.prev_loss) +
+                              (1 - self.trend_smoothing_factor) * self.trend)
+        elif self.smoothing == 'exp':
+            self.curr_loss = (self.data_smoothing_factor * loss +
+                              (1 - self.data_smoothing_factor) * self.curr_loss)
+        else:
+            self.curr_loss = loss
+        if self.curr_loss - self.best_loss < self.min_delta:
+            self.best_loss = self.curr_loss
+            self.waiting = 0
+        elif self.waiting >= self.patience:
+            raise StopIteration
+        else:
+            self.waiting += 1
