c2e348d839a05cd33907a22510caae7eff0ef989
Du Phan <fehiepsi@gmail.com>
2020-08-29 13:56:17 -0500

    cleanup tests

--- test/contrib/test_tfp.py
+++ test/contrib/test_tfp.py
@@ -32,11 +32,11 @@ def test_logistic_regression():
     from numpyro.contrib.tfp import distributions as dist
 
     N, dim = 3000, 3
-    num_warmup, num_samples = (1000, 8000)
+    num_warmup, num_samples = (1000, 1000)
     data = random.normal(random.PRNGKey(0), (N, dim))
     true_coefs = jnp.arange(1., dim + 1.)
     logits = jnp.sum(true_coefs * data, axis=-1)
-    labels = dist.Bernoulli(logits=logits).sample(seed=random.PRNGKey(1))
+    labels = dist.Bernoulli(logits=logits)(rng_key=random.PRNGKey(1))
 
     def model(labels):
         coefs = numpyro.sample('coefs', dist.Normal(jnp.zeros(dim), jnp.ones(dim)))
@@ -66,7 +66,7 @@ def test_beta_bernoulli():
         return p_latent
 
     true_probs = jnp.array([0.9, 0.1])
-    data = dist.Bernoulli(true_probs).sample(seed=random.PRNGKey(1), sample_shape=(1000, 2))
+    data = dist.Bernoulli(true_probs)(rng_key=random.PRNGKey(1), sample_shape=(1000, 2))
     kernel = NUTS(model=model, trajectory_length=0.1)
     mcmc = MCMC(kernel, num_warmup=warmup_steps, num_samples=num_samples)
     mcmc.run(random.PRNGKey(2), data)
