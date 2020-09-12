71f6b87e2c206ad8f6c7b2cb22a470d64c759dc1
Du Phan <fehiepsi@gmail.com>
2020-09-12 00:19:37 -0500

    cleanup the code

--- numpyro/infer/util.py
+++ numpyro/infer/util.py
@@ -427,12 +427,9 @@ def initialize_model(rng_key, model,
 
     if not_jax_tracer(is_valid):
         if device_get(~jnp.all(is_valid)):
-            with numpyro.validation_enabled(), \
-                    trace() as tr, \
-                    substitute(substitute_fn=init_strategy), \
-                    seed(rng_seed=rng_key if jnp.ndim(rng_key) == 1 else rng_key[0]):
+            with numpyro.validation_enabled(), trace() as tr:
                 # validate parameters
-                model(*model_args, **model_kwargs)
+                substituted_model(*model_args, **model_kwargs)
                 # validate values
                 for site in tr.values():
                     if site['type'] == 'sample':
