426721a8676e4399fa110d998abc453570767c97
Du Phan <fehiepsi@gmail.com>
2020-09-14 01:14:11 -0500

    add initial implementation of random module
    Co-authored-by: Tuan Nguyen <anhtuan277@gmail.com>

--- numpyro/contrib/module.py
+++ numpyro/contrib/module.py
@@ -1,6 +1,7 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
+from copy import deepcopy
 from functools import partial
 
 from jax import numpy as jnp
@@ -74,3 +75,34 @@ def haiku_module(name, nn, input_shape=None):
         nn_params = nn.init(rng_key, jnp.ones(input_shape))
         numpyro.param(module_key, nn_params)
     return partial(nn.apply, nn_params, None)
+
+
+def _update_params(params, new_params, prior, prefix=''):
+    for name, item in params.items():
+        flatten_name = ".".join([prefix, name]) if prefix else name
+        if isinstance(item, dict):
+            assert not isinstance(prior, dict) or flatten_name not in prior
+            new_item = new_params[name]
+            _update_params(item, new_item, prior, prefix=flatten_name)
+        elif not isinstance(prior, dict):
+            param_shape = jnp.shape(params[name])
+            params[name] = None
+            param_batch_shape = param_shape[:len(param_shape) - prior.event_dim]
+            new_params[name] = numpyro.sample(
+                flatten_name, prior.expand(param_batch_shape).to_event())
+        elif flatten_name in prior:
+            param_shape = jnp.shape(params[name])
+            params[name] = None
+            param_batch_shape = param_shape[:len(param_shape) - prior[flatten_name].event_dim]
+            new_params[name] = numpyro.sample(
+                flatten_name, prior[flatten_name].expand(param_batch_shape).to_event())
+
+
+def random_module(name, nn, prior, input_shape=None):
+    nn = flax_module(name, nn, input_shape)
+    params = nn.args[0]
+    new_params = deepcopy(params)
+    with numpyro.handlers.scope(prefix=name):
+        _update_params(params, new_params, prior, name)
+    nn_new = partial(nn.func, new_params, *nn.args[1:], **nn.keywords)
+    return nn_new
