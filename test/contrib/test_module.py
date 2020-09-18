2e5d848146f7e3138532bf0c13496c6848d091b1
Du Phan <fehiepsi@gmail.com>
2020-09-17 21:45:43 -0500

    update subsample test

--- test/contrib/test_module.py
+++ test/contrib/test_module.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-from jax import lax, random
+from jax import jit, lax, random
 from jax.nn import softplus
 
 import numpyro
@@ -72,26 +72,33 @@ def test_random_module():
         return x, y
 
     n_train_data = 5000
+    n_test_data = 100
     train_x, train_y = generate_data(n_train_data)
+    test_x, test_y = generate_data(n_test_data)
+    train_y = train_y / np.std(train_y)
+    test_y = test_y / np.std(test_y)
 
-    def model(x, y):
+    def model(x, y, batch_size=None):
         module = Model.partial(n_units=32)
-        nn = random_module("nn", module, dist.Normal(0, 1e-1), input_shape=(1, 1))
-        mean, rho = nn(x)
-        sigma = softplus(rho)
-        numpyro.sample("y", dist.Normal(mean, sigma), obs=y)
+        nn = random_module("nn", module, dist.Normal(0, 1e-1), input_shape=(1,))
+        with numpyro.plate("batch", x.shape[0], subsample_size=batch_size):
+            batch_x = numpyro.subsample(x, event_dim=1)
+            batch_y = numpyro.subsample(y, event_dim=1)
+            mean, rho = nn(batch_x)
+            sigma = softplus(rho)
+            numpyro.sample("obs", dist.Normal(mean, sigma), obs=batch_y)
 
     adam = optim.Adam(5e-3)
     guide = AutoDiagonalNormal(model)
     svi = SVI(model, guide, adam, ELBO())
-    # TODO: prepare data in batch
-    svi_state = svi.init(random.PRNGKey(1), train_x, train_y)
+    svi_state = svi.init(random.PRNGKey(0), train_x, train_y)
+    update_fn = jit(svi.update)
 
-    def body_fn(i, val):
-        svi_state, loss = svi.update(val, train_x, train_y)
-        return svi_state
+    n_iterations = 30000
+    batch_size = 256
+    for e in range(n_iterations):
+        svi_state, loss = update_fn(svi_state, train_x, train_y, batch_size=batch_size)
 
-    svi_state = lax.fori_loop(0, 3000, body_fn, svi_state)
     params = svi.get_params(svi_state)
     # TODO: predict something from params
     assert set(params.keys()) == set(["auto_loc", "auto_scale", "nn$params"])
