35ee81e20e4f93ec6f429f3335835951a4cc6296
Du Phan <fehiepsi@gmail.com>
2020-09-24 00:51:06 -0500

    fix wrong pip condition for jax

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     author_email='npradhan@uber.com',
     install_requires=[
         # TODO: pin to a specific version for the release (until JAX's API becomes stable)
-        'jax>=0.1.73<0.2.0',
+        'jax>=0.1.73<0.2',
         # check min version here: https://github.com/google/jax/blob/master/jax/lib/__init__.py#L20
         'jaxlib>=0.1.51',
         'tqdm',
