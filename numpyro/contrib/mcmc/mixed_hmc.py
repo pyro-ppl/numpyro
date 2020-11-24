60ed7d7ed1ce264f54e7bef3947b76192370082d
Du Phan <fehiepsi@gmail.com>
2020-11-24 11:42:43 -0600

    sketch the plan

---
+++ numpyro/contrib/mcmc/mixed_hmc.py
@@ -0,0 +1,5 @@
+# Sketch the prototype
+# + sketch a structure like hmc_ecs prototype
+# + implement discrete step functionality
+# + use enum for non-event discrete sites and mixed for event discrete sites
+# + test on gmm potential
