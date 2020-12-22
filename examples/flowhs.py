fix broadcasting in func.py

examples/flowhs.py
@@ -258,15 +258,15 @@ def predict(X, rng_key, omega, alpha, beta, tau_obs):
 def main(args):
     print(args)
 
-    X, Y = get_data(N=args.num_data, P=args.P)
+    N = args.num_data
+    X, Y = get_data(N=2 * N, P=args.P)
     #X, Y, expression = get_cancer_data()
     #X = X[:, :5000]
     #Y = Y[:500]
     #X = X[:500]
 
     #splits = KFold(args.num_folds, random_state=0, shuffle=True).split(X)
-    N = args.num_data
-    splits = [(np.arange(N//2), np.arange(N//2, N))]
+    splits = [(np.arange(N), np.arange(N, 2 * N))]
     out = np.zeros(X.shape[0])
 
     for split, (train, test) in enumerate(splits):
@@ -283,7 +283,7 @@ def main(args):
         rng_key_predict = random.split(rng_key_predict, args.num_samples)
 
         alpha = samples['alpha'] if args.model == 'flow' else jnp.zeros(args.num_samples)
-        beta = jnp.zeros(args.num_samples)
+        beta = samples['beta'] if args.model == 'flow' else jnp.zeros(args.num_samples)
         predictions = vmap(partial(predict, X_test))(rng_key_predict, samples['omega'], alpha,
                                                      beta, samples['tau_obs'])
         test_mean = jnp.mean(predictions, axis=0)
@@ -326,15 +326,15 @@ def main(args):
 if __name__ == "__main__":
     assert numpyro.__version__.startswith('0.4.1')
     parser = argparse.ArgumentParser(description="non-linear horseshoe")
-    parser.add_argument("-n", "--num-samples", default=1000, type=int)
+    parser.add_argument("-n", "--num-samples", default=4000, type=int)
     parser.add_argument("--num-warmup", default=1000, type=int)
     parser.add_argument("--truncation", default=0, type=int)
     parser.add_argument("--num-chains", default=1, type=int)
     parser.add_argument("--split", default=0, type=int)
-    parser.add_argument("--num-data", default=128, type=int)
+    parser.add_argument("--num-data", default=256, type=int)
     parser.add_argument("--num-folds", default=1, type=int)
     parser.add_argument("--strategy", default="gibbs", type=str, choices=["nuts", "gibbs"])
-    parser.add_argument("--P", default=8, type=int)
+    parser.add_argument("--P", default=128, type=int)
     parser.add_argument("--device", default='cpu', type=str, help='use "cpu" or "gpu".')
     parser.add_argument("--model", default='flow', type=str, choices=['flow', 'linear'])
     args = parser.parse_args()

examples/func.py
@@ -2,16 +2,10 @@ from functools import partial
 
 import numpy as np
 
-from jax import grad, vmap, jit, jvp, vjp, custom_jvp
+from jax import grad, vmap, jit, jvp, vjp, custom_jvp, value_and_grad
 import jax.numpy as jnp
 import jax.random as random
 from jax.lax import while_loop, stop_gradient, dynamic_slice_in_dim
-
-
-#def forward(alpha, x):
-#    return x + (2.0 / 3.0) * alpha * jnp.power(x, 3.0) + 0.2 * jnp.square(alpha) * jnp.power(x, 5.0)
-#def forward(alpha, beta, x):
-#    return x + (2.0 / 3.0) * alpha * jnp.power(x, 3.0) + 0.2 * jnp.square(alpha) * jnp.power(x, 5.0)
 
 
 def forward(alpha, beta, x):
@@ -21,27 +15,28 @@ def forward(alpha, beta, x):
 
 
 def cond_fn(val):
-    return (val[2] > 1.0e-6) & (val[3] < 200)
+    return (val[2] > 1.0e-6) & (val[3] < 100)
 
 
 def body_fn(alpha, beta, val):
     x, y, _, i = val
     f = partial(forward, alpha, beta)
-    df = grad(f)
-    delta = (f(x) - y) / df(x)
+    fx, dfx = value_and_grad(f)(x)
+    delta = (fx - y) / dfx
     x = x - delta
     return (x, y, jnp.fabs(delta), i + 1)
 
 
-@custom_jvp
+@partial(custom_jvp, nondiff_argnums=(2,))
 def inverse(alpha, beta, y):
     return while_loop(cond_fn, partial(body_fn, alpha, beta), (y, y, 9.9e9, 0))[0]
 
 
 @inverse.defjvp
-def inverse_jvp(primals, tangents):
-  alpha, beta, y = primals
-  alpha_dot, beta_dot, y_dot = tangents
+@jit
+def inverse_jvp(y, primals, tangents):
+  alpha, beta = primals
+  alpha_dot, beta_dot = tangents
   primal_out = inverse(alpha, beta, y)
   y_tilde = primal_out
   denominator = 1.0 + jnp.square(alpha * y_tilde + beta * jnp.square(y_tilde))
@@ -52,8 +47,10 @@ def inverse_jvp(primals, tangents):
   return primal_out, tangent_out
 
 
+
+@jit
 def jacobian_and_inverse(alpha, beta, Y):
-    Y_tilde = vmap(lambda y: inverse(alpha, beta, y))(Y)
+    Y_tilde = vmap(inverse)(jnp.broadcast_to(alpha, Y.shape), jnp.broadcast_to(beta, Y.shape), Y)
     Y_tilde_stop = stop_gradient(Y_tilde)
     log_det_jacobian = -jnp.sum(jnp.log(1.0 + jnp.square(alpha * Y_tilde_stop + beta * jnp.square(Y_tilde_stop))))
     return Y_tilde, log_det_jacobian
@@ -61,30 +58,43 @@ def jacobian_and_inverse(alpha, beta, Y):
 
 # tests
 if __name__ == '__main__':
-    Y = np.random.randn(2)
-    alpha = np.random.randn(1)[0]
-    beta = np.zeros(1)[0]
+    Y = 3 * np.random.randn(2)
+    alpha = 0.5 + 0.5 * np.random.rand(1)[0]
+    beta = 0.5 + 0.5 * np.random.rand(1)[0]
+    print("alpha, beta", alpha, beta)
     Y_tilde, jac = jacobian_and_inverse(alpha, beta, Y)
-    ffY = forward(alpha, beta, Y_tilde)
+    ffY = vmap(partial(forward, alpha, beta))(Y_tilde)
     delta = np.fabs(Y - ffY)
     print("Ytilde", Y_tilde, "Y", Y, "ffY", ffY, "delta", delta)
 
-    def Yt(Y, alpha):
+    def Yt(alpha, beta):
         return jacobian_and_inverse(alpha, beta, Y)[0][0]
 
-    g = grad(Yt, argnums=1)(Y, alpha)
+    g = grad(Yt, argnums=0)(alpha, beta)
     print("dYtilde/dalpha", g)
+    g = grad(Yt, argnums=1)(alpha, beta)
+    print("dYtilde/dbeta", g)
 
-    def Yt(Y, alpha):
+    def Yt(alpha, beta):
         return jacobian_and_inverse(alpha, beta, Y)[1]
 
-    g = grad(Yt, argnums=1)(Y, alpha)
+    g = grad(Yt, argnums=0)(alpha, beta)
     print("dlogjac/dalpha", g)
+    g = grad(Yt, argnums=1)(alpha, beta)
+    print("dlogjac/dbeta", g)
 
-    def YYt(Y, alpha, beta):
+    def YYt(alpha, beta):
         return jnp.sum(forward(alpha, beta, jacobian_and_inverse(alpha, beta, Y)[0]))
 
-    g = grad(YYt, argnums=1)(Y, alpha, beta)
+    g = grad(YYt, argnums=0)(alpha, beta)
     print("didentity/dalpha", g)
-    g = grad(YYt, argnums=2)(Y, alpha, beta)
+    g = grad(YYt, argnums=1)(alpha, beta)
     print("didentity/dbeta", g)
+
+    #def YYt(alpha, beta):
+    #    return jnp.sum(jacobian_and_inverse(alpha, beta, forward(alpha, beta, Y))[0])
+
+    #g = grad(YYt, argnums=0)(alpha, beta)
+    #print("didentity/dalpha", g)
+    #g = grad(YYt, argnums=1)(alpha, beta)
+    #print("didentity/dbeta", g)