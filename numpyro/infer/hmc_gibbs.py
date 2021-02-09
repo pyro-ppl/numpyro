116a63610468e0caed8f993f86fc08a863c9f450
Du Phan <fehiepsi@gmail.com>
2021-02-08 22:51:11 -0600

    skip doctest for print_summary

--- numpyro/infer/hmc_gibbs.py
+++ numpyro/infer/hmc_gibbs.py
@@ -325,7 +325,7 @@ class DiscreteHMCGibbs(HMCGibbs):
         >>> kernel = DiscreteHMCGibbs(NUTS(model), modified=True)
         >>> mcmc = MCMC(kernel, 1000, 100000, progress_bar=False)
         >>> mcmc.run(random.PRNGKey(0), probs, locs)
-        >>> mcmc.print_summary()
+        >>> mcmc.print_summary()  # doctest: +SKIP
         >>> samples = mcmc.get_samples()["x"]
         >>> assert abs(jnp.mean(samples) - 1.3) < 0.1
         >>> assert abs(jnp.var(samples) - 4.36) < 0.5
