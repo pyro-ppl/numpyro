d57b755140773659d157a499a76229a8c9f999ee
Du Phan <fehiepsi@gmail.com>
2021-02-13 17:45:51 -0600

    temp save

--- numpyro/infer/elbo.py
+++ numpyro/infer/elbo.py
@@ -77,6 +77,69 @@ class Trace_ELBO:
             return - jnp.mean(vmap(single_particle_elbo)(rng_keys))
 
 
+class TraceGraph_ELBO:
+    """
+    A trace implementation of ELBO-based SVI. The estimator is constructed
+    along the lines of references [1] and [2]. There are no restrictions on the
+    dependency structure of the model or the guide.
+
+    This is the most basic implementation of the Evidence Lower Bound, which is the
+    fundamental objective in Variational Inference. This implementation has various
+    limitations (for example it only supports random variables with reparameterized
+    samplers) but can be used as a template to build more sophisticated loss
+    objectives.
+
+    For more details, refer to http://pyro.ai/examples/svi_part_i.html.
+
+    **References:**
+
+    1. *Automated Variational Inference in Probabilistic Programming*,
+       David Wingate, Theo Weber
+    2. *Black Box Variational Inference*,
+       Rajesh Ranganath, Sean Gerrish, David M. Blei
+
+    :param num_particles: The number of particles/samples used to form the ELBO
+        (gradient) estimators.
+    """
+    def __init__(self, num_particles=1):
+        self.num_particles = num_particles
+
+    def loss(self, rng_key, param_map, model, guide, *args, **kwargs):
+        """
+        Evaluates the ELBO with an estimator that uses num_particles many samples/particles.
+
+        :param jax.random.PRNGKey rng_key: random number generator seed.
+        :param dict param_map: dictionary of current parameter values keyed by site
+            name.
+        :param model: Python callable with NumPyro primitives for the model.
+        :param guide: Python callable with NumPyro primitives for the guide.
+        :param args: arguments to the model / guide (these can possibly vary during
+            the course of fitting).
+        :param kwargs: keyword arguments to the model / guide (these can possibly vary
+            during the course of fitting).
+        :return: negative of the Evidence Lower Bound (ELBO) to be minimized.
+        """
+        def single_particle_elbo(rng_key):
+            model_seed, guide_seed = random.split(rng_key)
+            seeded_model = seed(model, model_seed)
+            seeded_guide = seed(guide, guide_seed)
+            guide_log_density, guide_trace = log_density(seeded_guide, args, kwargs, param_map)
+            seeded_model = replay(seeded_model, guide_trace)
+            model_log_density, _ = log_density(seeded_model, args, kwargs, param_map)
+
+            # log p(z) - log q(z)
+            elbo = model_log_density - guide_log_density
+            return elbo
+
+        # Return (-elbo) since by convention we do gradient descent on a loss and
+        # the ELBO is a lower bound that needs to be maximized.
+        if self.num_particles == 1:
+            return - single_particle_elbo(rng_key)
+        else:
+            rng_keys = random.split(rng_key, self.num_particles)
+            return - jnp.mean(vmap(single_particle_elbo)(rng_keys))
+
+
 class ELBO(Trace_ELBO):
     def __init__(self, num_particles=1):
         warnings.warn("Using ELBO directly in SVI is deprecated. Please use Trace_ELBO class instead.",
