8418d768917d356b80274ea92d9bfbbcdc4ce3f9
Martin Jankowiak <jankowiak@gmail.com>
2021-02-20 19:18:07 -0500

    wip2

--- numpyro/infer/barker.py
+++ numpyro/infer/barker.py
@@ -178,7 +178,12 @@ class BarkerMH(MCMCKernel):
         rng_key, key_normal, key_bernoulli, key_accept = random.split(rng_key, 4)
 
         step_size = adapt_state.step_size
-        mass_sqrt = step_size * adapt_state.mass_matrix_sqrt
+
+        if self._dense_mass:
+            cov = jnp.array([[1.0, 0.97], [0.97, 1.0]])
+            mass_sqrt = step_size * jax.scipy.linalg.cholesky(cov, lower=True)
+        else:
+            mass_sqrt = step_size * adapt_state.mass_matrix_sqrt
 
         # Generate proposal y.
         z = random.normal(key_normal, shape)
