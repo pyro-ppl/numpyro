eb357db45b3018445aba95f71dba54a99487acba
kpj <kim.philipp.jablonski@gmail.com>
2021-03-12 12:36:17 +0100

    Add test for link between discrete and continuous distributions

--- test/test_model_rendering.py
+++ test/test_model_rendering.py
@@ -26,6 +26,11 @@ def nested_plates():
         z = numpyro.sample('z', dist.Normal(0, 1))
 
 
+def discrete_to_continuous(probs, locs):
+    c = numpyro.sample('c', dist.Categorical(probs))
+    numpyro.sample('x', dist.Normal(locs[c], 0.5))
+
+
 @pytest.mark.parametrize('test_model,model_kwargs,expected_graph_spec', [
     (simple, dict(data=jnp.ones(10)), {
         'plate_groups': {'N': ['obs'], None: ['x', 'sd']},
@@ -50,7 +55,20 @@ def nested_plates():
             'z': {'is_observed': False},
         },
         'edge_list': [],
-    })
+    }),
+    (
+        discrete_to_continuous,
+        dict(probs=jnp.array([0.15, 0.3, 0.3, 0.25]), locs=jnp.array([-2, 0, 2, 4])),
+        {
+            'plate_groups': {None: ['c', 'x']},
+            'plate_data': {},
+            'node_data': {
+                'c': {'is_observed': False},
+                'x': {'is_observed': False},
+            },
+            'edge_list': [('c', 'x')],
+        }
+    ),
 ])
 def test_model_transformation(test_model, model_kwargs, expected_graph_spec):
     relations = get_model_relations(test_model, **model_kwargs)
