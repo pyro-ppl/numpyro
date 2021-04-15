d0f9a46e81d4dae79a49cb4f5d18354a6587c961
Hessam Mehr <hessammehr@gmail.com>
2021-04-15 20:10:18 +0100

    `soft_vmap`: Don't reshape empty tensors.

--- numpyro/util.py
+++ numpyro/util.py
@@ -392,6 +392,6 @@ def soft_vmap(fn, xs, batch_ndims=1, chunk_size=None):
     ys = lax.map(fn, xs) if num_chunks > 1 else fn(xs)
     map_ndims = int(num_chunks > 1) + int(chunk_size > 1)
     ys = tree_map(
-        lambda y: jnp.reshape(y, (-1,) + jnp.shape(y)[map_ndims:])[:batch_size], ys
+        lambda y: jnp.reshape(y, (-1,) + jnp.shape(y)[map_ndims:])[:batch_size] if y.size else y, ys
     )
-    return tree_map(lambda y: jnp.reshape(y, batch_shape + jnp.shape(y)[1:]), ys)
+    return tree_map(lambda y: jnp.reshape(y, batch_shape + jnp.shape(y)[1:]) if y.size else y, ys)
