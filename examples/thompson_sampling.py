fe0f744274160cdbbe63a5f1e3be39a44e4a7ea8
Dominik Straub <dominikstrb@mailbox.org>
2021-04-20 14:20:00 +0200

    Adhere to style guid

--- examples/thompson_sampling.py
+++ examples/thompson_sampling.py
@@ -17,15 +17,18 @@ numpyro.enable_x64()
 
 # defining the function. At y=0 to get a 1D cut at the origin
 def ackley_1d(x, y=0):
-    out = (-20 * jnp.exp(-0.2 * jnp.sqrt(0.5 * (x ** 2 + y ** 2)))
-           - jnp.exp(0.5 * (jnp.cos(2 * jnp.pi * x) + jnp.cos(2 * jnp.pi * y)))
-           + jnp.e + 20)
+    out = (
+        -20 * jnp.exp(-0.2 * jnp.sqrt(0.5 * (x ** 2 + y ** 2)))
+        - jnp.exp(0.5 * (jnp.cos(2 * jnp.pi * x) + jnp.cos(2 * jnp.pi * y)))
+        + jnp.e
+        + 20
+    )
     return out
 
 
 # matern kernel with nu = 1.5
-def matern32_kernel(X, Z, var=1., length=.5, jitter=1.0e-6):
-    d = jnp.sqrt(.5) * jnp.sqrt(jnp.power((X[:, None] - Z), 2.0)) / length
+def matern32_kernel(X, Z, var=1.0, length=0.5, jitter=1.0e-6):
+    d = jnp.sqrt(0.5) * jnp.sqrt(jnp.power((X[:, None] - Z), 2.0)) / length
     k = var * (1 + d + (d ** 2) / 3) * jnp.exp(-d)
     if jitter:
         k += jitter * jnp.eye(X.shape[0])
@@ -34,8 +37,8 @@ def matern32_kernel(X, Z, var=1., length=.5, jitter=1.0e-6):
 
 def model(X, Y, kernel=matern32_kernel):
     # set uninformative log-normal priors on our three kernel hyperparameters
-    var = numpyro.sample("var", dist.LogNormal(0., 1.))
-    length = numpyro.sample("length", dist.LogNormal(0., 1.))
+    var = numpyro.sample("var", dist.LogNormal(0.0, 1.0))
+    length = numpyro.sample("length", dist.LogNormal(0.0, 1.0))
 
     # compute kernel
     k = kernel(X, X, var, length)
@@ -63,8 +66,14 @@ class GP:
 
         optim = numpyro.optim.Adam(step_size=0.005)
 
-        svi = SVI(model, guide=AutoDelta(model), optim=optim, loss=Trace_ELBO(),
-                  X=X, Y=(Y - self.y_mean) / self.y_std)
+        svi = SVI(
+            model,
+            guide=AutoDelta(model),
+            optim=optim,
+            loss=Trace_ELBO(),
+            X=X,
+            Y=(Y - self.y_mean) / self.y_std,
+        )
 
         params, _ = svi.run(rng_key, n_step)
 
@@ -82,14 +91,17 @@ class GP:
     def predict(self, X, return_std=False):
         # compute kernels between train and test data, etc.
         k_pp = self.kernel(X, X, **self.kernel_params)
-        k_pX = self.kernel(X, self.X_train, **self.kernel_params, jitter=0.)
+        k_pX = self.kernel(X, self.X_train, **self.kernel_params, jitter=0.0)
         K = k_pp - jnp.matmul(k_pX, jnp.matmul(self.K_xx_inv, jnp.transpose(k_pX)))
 
         mean = jnp.matmul(k_pX, self.alpha)
 
         # we return both the mean function and the standard deviation
         if return_std:
-            return (mean * self.y_std) + self.y_mean, jnp.sqrt(jnp.diag(K * self.y_std ** 2))
+            return (
+                (mean * self.y_std) + self.y_mean,
+                jnp.sqrt(jnp.diag(K * self.y_std ** 2)),
+            )
         else:
             return (mean * self.y_std) + self.y_mean, K * self.y_std ** 2
 
@@ -102,7 +114,9 @@ class GP:
 class ThompsonSamplingGP:
 
     # initialization
-    def __init__(self, gp, n_random_draws, objective, x_bounds, interval_resolution=1000):
+    def __init__(
+        self, gp, n_random_draws, objective, x_bounds, interval_resolution=1000
+    ):
         # Gaussian Process
         self.gp = gp
 
@@ -119,7 +133,9 @@ class ThompsonSamplingGP:
         # represent the posterior sample
         # we also define the x grid
         self.interval_resolution = interval_resolution
-        self.X_grid = np.linspace(self.bounds[0], self.bounds[1], self.interval_resolution)
+        self.X_grid = np.linspace(
+            self.bounds[0], self.bounds[1], self.interval_resolution
+        )
 
         # also initializing our design matrix and target variable
         self.X = np.array([])
@@ -158,7 +174,9 @@ class ThompsonSamplingGP:
             next_sample = self.X_grid[which_min]
 
             # let us also get the std from the posterior, for visualization purposes
-            posterior_mean, posterior_std = self.gp.predict(self.X_grid, return_std=True)
+            posterior_mean, posterior_std = self.gp.predict(
+                self.X_grid, return_std=True
+            )
 
         # let us observe the objective and append this new data to our X and y
         next_observation = self.objective(next_sample)
@@ -168,43 +186,83 @@ class ThompsonSamplingGP:
         # return everything if possible
         try:
             # returning values of interest
-            return self.X, self.y, self.X_grid, posterior_sample, posterior_mean, posterior_std
+            return (
+                self.X,
+                self.y,
+                self.X_grid,
+                posterior_sample,
+                posterior_mean,
+                posterior_std,
+            )
 
         # if not, return whats possible to return
-        except:
-            return (self.X, self.y, self.X_grid, np.array([np.mean(self.y)] * self.interval_resolution),
-                    np.array([np.mean(self.y)] * self.interval_resolution), np.array([0] * self.interval_resolution))
+        except UnboundLocalError:
+            return (
+                self.X,
+                self.y,
+                self.X_grid,
+                np.array([np.mean(self.y)] * self.interval_resolution),
+                np.array([np.mean(self.y)] * self.interval_resolution),
+                np.array([0] * self.interval_resolution),
+            )
 
 
 def main(args):
     gp = GP(kernel=matern32_kernel)
     # do inference
-    thompson = ThompsonSamplingGP(gp, n_random_draws=args.num_random, objective=ackley_1d, x_bounds=(-4, 4))
+    thompson = ThompsonSamplingGP(
+        gp, n_random_draws=args.num_random, objective=ackley_1d, x_bounds=(-4, 4)
+    )
 
     for i in range(20):
-        X, y, X_grid, posterior_sample, posterior_mean, posterior_std = thompson.choose_next_sample(
-            n_step=args.num_step,
-        )
+        (
+            X,
+            y,
+            X_grid,
+            posterior_sample,
+            posterior_mean,
+            posterior_std,
+        ) = thompson.choose_next_sample(n_step=args.num_step,)
 
         fig, ax = plt.subplots(figsize=(8, 6), constrained_layout=True)
 
         # plot training data
         ax.scatter(X, y, color="blue", marker="o", label="samples")
         ax.plot(X_grid, ackley_1d(X_grid), color="black", linestyle="--")
-        ax.plot(X_grid, posterior_sample, color="red", linestyle="-", label="posterior sample")
+        ax.plot(
+            X_grid,
+            posterior_sample,
+            color="red",
+            linestyle="-",
+            label="posterior sample",
+        )
         # plot 90% confidence level of predictions
-        ax.fill_between(X_grid, posterior_mean - posterior_std, posterior_mean + posterior_std, color="red", alpha=.5)
+        ax.fill_between(
+            X_grid,
+            posterior_mean - posterior_std,
+            posterior_mean + posterior_std,
+            color="red",
+            alpha=0.5,
+        )
         ax.set(xlabel="X", ylabel="Y")
 
         plt.legend()
         plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     assert numpyro.__version__.startswith("0.6.0")
     parser = argparse.ArgumentParser(description="Thompson sampling example")
-    parser.add_argument("--num-random", nargs="?", default=2, type=int, help="number of random draws")
-    parser.add_argument("--num-step", nargs="?", default=2_000, type=int, help="number of steps for optimization")
+    parser.add_argument(
+        "--num-random", nargs="?", default=2, type=int, help="number of random draws"
+    )
+    parser.add_argument(
+        "--num-step",
+        nargs="?",
+        default=2_000,
+        type=int,
+        help="number of steps for optimization",
+    )
     parser.add_argument("--device", default="cpu", type=str, help='use "cpu" or "gpu".')
     args = parser.parse_args()
 
