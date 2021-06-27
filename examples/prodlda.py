d78500720f4ee766407ab0f7a8d42de02f05d68e
tcbegley <tomcbegley@gmail.com>
2021-06-27 14:59:55 +0100

    Add ProdLDA example

---
+++ examples/prodlda.py
@@ -0,0 +1,214 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Example: ProdLDA
+================
+In this example, we will follow [1] to implement the ProdLDA topic model from
+Autoencoding Variational Inference For Topic Models by Akash Srivastava and
+Charles Sutton. This model returns consistently better topics than vanilla LDA
+and trains much more quickly. Furthermore, it does not require a custom
+inference algorithm that relies on complex mathematical derivations. This
+example also serves as an introduction to Flax and Haiku modules in NumPyro.
+
+**References:**
+    1. http://pyro.ai/examples/prodlda.html
+    2. Akash Srivastava, & Charles Sutton. (2017). Autoencoding Variational
+       Inference For Topic Models.
+.. image:: https://i.ibb.co/zV5rjX6/Screen-Shot-2020-09-24-at-11-21-38.png
+    :align: center
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from wordcloud import WordCloud
+
+import haiku as hk
+import jax
+from jax import device_put, random
+import jax.numpy as jnp
+
+import numpyro
+from numpyro.contrib.module import haiku_module
+import numpyro.distributions as dist
+from numpyro.infer import SVI, TraceMeanField_ELBO
+
+
+class HaikuEncoder:
+    def __init__(self, vocab_size, num_topics, hidden, dropout_rate):
+        self._dropout_rate = dropout_rate
+        self._vocab_size = vocab_size
+        self._hidden = hidden
+        self._num_topics = num_topics
+
+    def __call__(self, inputs, is_training):
+        dropout_rate = self._dropout_rate if is_training else 0.0
+
+        h = jax.nn.softplus(hk.Linear(self._hidden)(inputs))
+        h = jax.nn.softplus(hk.Linear(self._hidden)(h))
+        h = hk.dropout(hk.next_rng_key(), dropout_rate, h)
+
+        # NB: here we set `create_scale=False` and `create_offset=False` to reduce
+        # the number of learning parameters
+        h_mu = hk.Linear(self._num_topics)(h)
+        logtheta_loc = hk.BatchNorm(
+            create_scale=False, create_offset=False, decay_rate=0.1
+        )(h_mu, is_training)
+
+        h_sigma = hk.Linear(self._num_topics)(h)
+        logtheta_logvar = hk.BatchNorm(
+            create_scale=False, create_offset=False, decay_rate=0.1
+        )(h_sigma, is_training)
+        logtheta_scale = jnp.exp(0.5 * logtheta_logvar)
+        return logtheta_loc, logtheta_scale
+
+
+class HaikuDecoder:
+    def __init__(self, vocab_size, dropout_rate):
+        self._dropout_rate = dropout_rate
+        self._vocab_size = vocab_size
+
+    def __call__(self, inputs, is_training):
+        dropout_rate = self._dropout_rate if is_training else 0.0
+        h = hk.dropout(hk.next_rng_key(), dropout_rate, inputs)
+        h = hk.Linear(self._vocab_size)(h)
+        h = hk.BatchNorm(
+            create_scale=True, create_offset=True, decay_rate=0.1
+        )(h, is_training)
+        return jax.nn.softmax(h)
+
+
+def model(docs, hyperparams, is_training=False):
+    decoder = haiku_module(
+        "decoder",
+        # use `transform_with_state` for BatchNorm
+        hk.transform_with_state(
+            HaikuDecoder(
+                hyperparams["vocab_size"], hyperparams["dropout_rate"]
+            )
+        ),
+        input_shape=(1, hyperparams["num_topics"]),
+        apply_rng=True,
+        # to ensure proper initialisation of BatchNorm we must
+        # initialise with is_training=True
+        is_training=True,
+    )
+
+    with numpyro.plate(
+        "documents", docs.shape[0], subsample_size=hyperparams["batch_size"]
+    ):
+        batch_docs = numpyro.subsample(docs, event_dim=1)
+        logtheta_loc = jnp.zeros(
+            (batch_docs.shape[0], hyperparams["num_topics"])
+        )
+        logtheta_scale = jnp.ones(
+            (batch_docs.shape[0], hyperparams["num_topics"])
+        )
+        logtheta = numpyro.sample(
+            "logtheta", dist.Normal(logtheta_loc, logtheta_scale).to_event(1)
+        )
+        theta = jax.nn.softmax(logtheta)
+
+        count_param = decoder(numpyro.prng_key(), theta, is_training)
+
+        total_count = batch_docs.sum(-1).max().astype(int)
+        numpyro.sample(
+            "obs", dist.Multinomial(total_count, count_param), obs=batch_docs
+        )
+
+
+def guide(docs, hyperparams, is_training=False):
+    encoder = haiku_module(
+        "encoder",
+        # use `transform_with_state` for BatchNorm
+        hk.transform_with_state(
+            HaikuEncoder(
+                hyperparams["vocab_size"],
+                hyperparams["num_topics"],
+                hyperparams["hidden"],
+                hyperparams["dropout_rate"],
+            )
+        ),
+        input_shape=(1, hyperparams["vocab_size"]),
+        apply_rng=True,
+        # to ensure proper initialisation of BatchNorm we must
+        # initialise with is_training=True
+        is_training=True,
+    )
+    with numpyro.plate(
+        "documents", docs.shape[0], subsample_size=hyperparams["batch_size"]
+    ):
+        batch_docs = numpyro.subsample(docs, event_dim=1)
+        logtheta_loc, logtheta_scale = encoder(
+            numpyro.prng_key(), batch_docs, is_training
+        )
+        numpyro.sample(
+            "logtheta", dist.Normal(logtheta_loc, logtheta_scale).to_event(1)
+        )
+
+
+def plot_word_cloud(b, ax, n):
+    indices = jnp.argsort(b)[::-1]
+    df = pd.DataFrame(indices[:100], columns=["index"])
+    words = pd.merge(df, vocab[["index", "word"]], how="left", on="index")[
+        "word"
+    ].values.tolist()
+    sizes = (b[indices[:100]] * 1000).astype(int).tolist()
+    freqs = {words[i]: sizes[i] for i in range(len(words))}
+    wc = WordCloud(background_color="white", width=800, height=500)
+    wc = wc.generate_from_frequencies(freqs)
+    ax.set_title("Topic %d" % (n + 1))
+    ax.imshow(wc, interpolation="bilinear")
+    ax.axis("off")
+
+
+if __name__ == "__main__":
+    rng_key = random.PRNGKey(0)
+    # TODO: remove data_home override
+    news = fetch_20newsgroups(subset="all", data_home="/tmp/scikit-learn-data")
+    vectorizer = CountVectorizer(max_df=0.5, min_df=20, stop_words="english")
+    docs = jnp.array(vectorizer.fit_transform(news["data"]).toarray())
+
+    vocab = pd.DataFrame(columns=["word", "index"])
+    vocab["word"] = vectorizer.get_feature_names()
+    vocab["index"] = vocab.index
+
+    print(f"Dictionary size: {len(vocab)}")
+    print(f"Corpus size: {docs.shape}")
+
+    # TODO: allow these to be overridden with command-line arguments
+    rng_key = random.PRNGKey(0)
+    num_topics = 20
+    docs = device_put(docs)
+    batch_size = 32
+    learning_rate = 1e-3
+    num_steps = 200_000
+    hidden = 100
+
+    hyperparams = dict(
+        vocab_size=docs.shape[1],
+        num_topics=num_topics,
+        hidden=hidden,
+        dropout_rate=0.2,
+        batch_size=batch_size,
+    )
+
+    optimizer = numpyro.optim.Adam(learning_rate)
+    svi = SVI(model, guide, optimizer, loss=TraceMeanField_ELBO())
+
+    params, losses = svi.run(
+        rng_key, num_steps, docs, hyperparams, is_training=True
+    )
+
+    plt.plot(losses)
+
+    beta = params["decoder$params"]["haiku_decoder/linear"]["w"]
+
+    fig, axs = plt.subplots(7, 3, figsize=(14, 24))
+    for n in range(beta.shape[0]):
+        i, j = divmod(n, 3)
+        plot_word_cloud(beta[n], axs[i, j], vocab, n)
+    axs[-1, -1].axis("off")
+
+    plt.show()
