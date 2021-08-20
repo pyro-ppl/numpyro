31f7924f990e8a2401ba18115f4ad481cf1e4a3d
Nicholas Mancuso <nick.mancuso@gmail.com>
2021-08-19 21:29:03 -0700

    fixed tests for SVI warning

--- test/pyroapi/test_pyroapi.py
+++ test/pyroapi/test_pyroapi.py
@@ -1,13 +1,27 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
+from numpyro.infer import (
+    RenyiELBO,
+    Trace_ELBO,
+    TraceMeanField_ELBO,
+)
 from pyroapi import pyro_backend
 from pyroapi.tests import *  # noqa F401
 import pytest
 
+cont_inf_only_cls_names = [
+    RenyiELBO.__name__,
+    Trace_ELBO.__name__,
+    TraceMeanField_ELBO.__name__,
+]
+
 pytestmark = pytest.mark.filterwarnings(
     "ignore::numpyro.compat.util.UnsupportedAPIWarning",
-    "ignore:Currently, SVI does not support models with discrete",
+    *(
+        f"ignore:Currently, SVI with {s_name} loss does not support models with discrete latent variables"
+        for s_name in cont_inf_only_cls_names
+    ),
 )
 
 
