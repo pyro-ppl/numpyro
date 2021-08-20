9086ea5ad89f04d0bd168346a720e6ec6897d0c2
Nicholas Mancuso <nick.mancuso@gmail.com>
2021-08-19 21:33:11 -0700

    isort warning

--- test/pyroapi/test_pyroapi.py
+++ test/pyroapi/test_pyroapi.py
@@ -1,14 +1,11 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
-from numpyro.infer import (
-    RenyiELBO,
-    Trace_ELBO,
-    TraceMeanField_ELBO,
-)
 from pyroapi import pyro_backend
 from pyroapi.tests import *  # noqa F401
 import pytest
+
+from numpyro.infer import RenyiELBO, Trace_ELBO, TraceMeanField_ELBO
 
 cont_inf_only_cls_names = [
     RenyiELBO.__name__,
