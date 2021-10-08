61cdf5b83b332e610da3ab7c4ec5c4787a1e80b7
Amal Vaidya <amal.vaidya@faculty.ai>
2021-10-08 13:56:09 +0000

    add test for scope frames

--- test/test_handlers.py
+++ test/test_handlers.py
@@ -591,6 +591,26 @@ def test_scope():
     assert "b/a/x" in trace
 
 
+def test_scope_frames():
+    def model(y):
+        mu = numpyro.sample("mu", dist.Normal())
+        sigma = numpyro.sample("sigma", dist.HalfNormal())
+
+        with numpyro.plate("plate1", y.shape[0]):
+            numpyro.sample("y", dist.Normal(mu, sigma), obs=y)
+
+    scope_prefix = "scope"
+    scoped_model = handlers.scope(model, prefix=scope_prefix)
+
+    obs = np.random.normal(size=(10,))
+
+    trace = handlers.trace(handlers.seed(model, 0)).get_trace(obs)
+    scoped_trace = handlers.trace(handlers.seed(scoped_model, 0)).get_trace(obs)
+
+    assert trace["y"]["cond_indep_stack"][0].name in trace
+    assert scoped_trace[f"{scope_prefix}/y"]["cond_indep_stack"][0].name in scoped_trace
+
+
 def test_lift():
     def model():
         loc1 = numpyro.param("loc1", 0.0)
