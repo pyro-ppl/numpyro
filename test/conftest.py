63f54587b521bec5cc77cc6eabc9d0ec583443e4
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-10-13 00:12:59 -0400

    Fix typo in conftest.py (#1188)

--- test/conftest.py
+++ test/conftest.py
@@ -11,6 +11,6 @@ config.update("jax_platform_name", "cpu")  # noqa: E702
 
 
 def pytest_runtest_setup(item):
-    if "JAX_ENABLE_x64" in os.environ:
+    if "JAX_ENABLE_X64" in os.environ:
         config.update("jax_enable_x64", True)
     set_rng_seed(0)
