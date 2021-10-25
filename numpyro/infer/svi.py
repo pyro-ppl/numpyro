bc05e6c87a497c5f656fa5b43f43058fb5f35333
Ben <bjeffrey92@hotmail.com>
2021-10-25 10:16:24 +0100

    remove trailing whitespace

--- numpyro/infer/svi.py
+++ numpyro/infer/svi.py
@@ -309,10 +309,10 @@ class SVI(object):
             final state of previous SVI run. Usage::
 
                 svi = SVI(model, guide, optimizer, loss=Trace_ELBO())
-                svi_result = svi.run(random.PRNGKey(0), 2000, data) 
+                svi_result = svi.run(random.PRNGKey(0), 2000, data)
                 # upon inspection of svi_result the user decides that the model has not converged
                 # continue from the end of the previous svi run rather than beginning again from iteration 0
-                svi_result = svi.run(random.PRNGKey(1), 2000, data, init_state=svi_result.state) 
+                svi_result = svi.run(random.PRNGKey(1), 2000, data, init_state=svi_result.state)
 
         :param kwargs: keyword arguments to the model / guide
         :return: a namedtuple with fields `params` and `losses` where `params`
