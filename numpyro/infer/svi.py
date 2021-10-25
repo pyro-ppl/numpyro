965daa369810d61273415fa10a76f406ec3d4442
Ben Jeffrey <36240394+bjeffrey92@users.noreply.github.com>
2021-10-25 14:45:13 +0100

    Option to use state of previous SVI run as starting state (#1194)
    * argument to SVI.run() which takes the state of previous SVI run as starting point
    
    * change argument name and type hint
    
    * usage in documentation
    
    * remove trailing whitespace

--- numpyro/infer/svi.py
+++ numpyro/infer/svi.py
@@ -285,6 +285,7 @@ class SVI(object):
         *args,
         progress_bar=True,
         stable_update=False,
+        init_state=None,
         **kwargs,
     ):
         """
@@ -304,6 +305,15 @@ class SVI(object):
             ``True``.
         :param bool stable_update: whether to use :meth:`stable_update` to update
             the state. Defaults to False.
+        :param SVIState init_state: if not None, begin SVI from the
+            final state of previous SVI run. Usage::
+
+                svi = SVI(model, guide, optimizer, loss=Trace_ELBO())
+                svi_result = svi.run(random.PRNGKey(0), 2000, data)
+                # upon inspection of svi_result the user decides that the model has not converged
+                # continue from the end of the previous svi run rather than beginning again from iteration 0
+                svi_result = svi.run(random.PRNGKey(1), 2000, data, init_state=svi_result.state)
+
         :param kwargs: keyword arguments to the model / guide
         :return: a namedtuple with fields `params` and `losses` where `params`
             holds the optimized values at :class:`numpyro.param` sites,
@@ -321,7 +331,10 @@ class SVI(object):
                 svi_state, loss = self.update(svi_state, *args, **kwargs)
             return svi_state, loss
 
-        svi_state = self.init(rng_key, *args, **kwargs)
+        if init_state is None:
+            svi_state = self.init(rng_key, *args, **kwargs)
+        else:
+            svi_state = init_state
         if progress_bar:
             losses = []
             with tqdm.trange(1, num_steps + 1) as t:
