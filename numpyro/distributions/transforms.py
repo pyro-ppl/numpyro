128959bd89c08c54e8c1c5ca4bbe860ec3da42d5
MarcoGorelli <marcogorelli@protonmail.com>
2021-11-27 08:54:21 +0000

    deduplicate _transform_to_interval

--- numpyro/distributions/transforms.py
+++ numpyro/distributions/transforms.py
@@ -1106,23 +1106,11 @@ def _biject_to_independent(constraint):
     )
 
 
+@biject_to.register(constraints.open_interval)
 @biject_to.register(constraints.interval)
 def _transform_to_interval(constraint):
     if constraint is constraints.unit_interval:
         return SigmoidTransform()
-    scale = constraint.upper_bound - constraint.lower_bound
-    return ComposeTransform(
-        [
-            SigmoidTransform(),
-            AffineTransform(
-                constraint.lower_bound, scale, domain=constraints.unit_interval
-            ),
-        ]
-    )
-
-
-@biject_to.register(constraints.open_interval)
-def _transform_to_open_interval(constraint):
     scale = constraint.upper_bound - constraint.lower_bound
     return ComposeTransform(
         [
