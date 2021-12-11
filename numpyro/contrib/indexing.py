2c3fb7f5c08504f026d80af96bfcb354e1d9a5cb
Du Phan <fehiepsi@gmail.com>
2021-12-11 14:34:41 -0500

    make format

--- numpyro/contrib/indexing.py
+++ numpyro/contrib/indexing.py
@@ -1,9 +1,12 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
+import warnings
+
+from numpyro.ops import Vindex, vindex  # noqa: F401
+
 warnings.warn(
     "`indexing` module has been moved from `numpyro.contrib` to `numpyro.ops`."
     " Please import Vindex or vindex functions from `numpyro.ops.indexing`.",
     FutureWarning,
 )
-from numpyro.ops import Vindex, vindex
