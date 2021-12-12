d8fb03a51adbd52c4296d7481fe0224bb1495d1d
Du Phan <fehiepsi@gmail.com>
2021-12-11 23:53:51 -0500

    Fix circular test

--- test/infer/test_reparam.py
+++ test/infer/test_reparam.py
@@ -297,7 +297,7 @@ def test_circular(shape):
     # This model is the expected distribution
     def model_exp(loc, concentration):
         with numpyro.plate_stack("plates", shape):
-            with numpyro.plate("particles", 10000):
+            with numpyro.plate("particles", 20000):
                 numpyro.sample("x", dist.VonMises(loc, concentration))
 
     # This model is for inference
@@ -314,8 +314,8 @@ def test_circular(shape):
         return get_circular_moments(trace["x"]["value"])
 
     def get_actual_probe(loc, concentration):
-        kernel = NUTS(model_act)
-        mcmc = MCMC(kernel, num_warmup=1000, num_samples=10000, num_chains=1)
+        kernel = NUTS(model_act, dense_mass=True)
+        mcmc = MCMC(kernel, num_warmup=2000, num_samples=20000, num_chains=1)
         mcmc.run(random.PRNGKey(0), loc, concentration)
         samples = mcmc.get_samples()
         return get_circular_moments(samples["x"])
