66efebb8991e9d4f2bfef840dd6bb1f7ebaf59d0
OlaRonning <ronning@pm.me>
2021-12-28 21:30:12 +0100

    Forgot lint

--- test/contrib/einstein/test_reinit_guide.py
+++ test/contrib/einstein/test_reinit_guide.py
@@ -1,25 +1,41 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
 import pytest
+
 from jax import random
 
 import numpyro
 from numpyro import handlers
 from numpyro.contrib.einstein.reinit_guide import WrappedGuide
-from numpyro.distributions import Normal, Bernoulli
+from numpyro.distributions import Bernoulli, Normal
 from numpyro.distributions.constraints import _Real
-from numpyro.infer import init_to_feasible, init_to_median, init_to_sample, init_to_uniform
-from numpyro.infer.autoguide import AutoMultivariateNormal, AutoLaplaceApproximation, AutoLowRankMultivariateNormal, \
-    AutoNormal, AutoDelta, AutoDiagonalNormal
+from numpyro.infer import (
+    init_to_feasible,
+    init_to_median,
+    init_to_sample,
+    init_to_uniform,
+)
+from numpyro.infer.autoguide import (
+    AutoDelta,
+    AutoDiagonalNormal,
+    AutoLaplaceApproximation,
+    AutoLowRankMultivariateNormal,
+    AutoMultivariateNormal,
+    AutoNormal,
+)
 
 
 @pytest.mark.parametrize(
     "auto_class",
-    [AutoMultivariateNormal,
-     AutoLaplaceApproximation,
-     AutoLowRankMultivariateNormal,
-     AutoNormal,
-     AutoDelta,
-     AutoDiagonalNormal,
-     ]
+    [
+        AutoMultivariateNormal,
+        AutoLaplaceApproximation,
+        AutoLowRankMultivariateNormal,
+        AutoNormal,
+        AutoDelta,
+        AutoDiagonalNormal,
+    ],
 )
 @pytest.mark.parametrize(
     "init_loc_fn",
@@ -30,14 +46,13 @@ from numpyro.infer.autoguide import AutoMultivariateNormal, AutoLaplaceApproxima
         init_to_uniform,
     ],
 )
-@pytest.mark.parametrize(
-    "num_particles", [1, 2, 10])
+@pytest.mark.parametrize("num_particles", [1, 2, 10])
 def test_auto_guide(auto_class, init_loc_fn, num_particles):
     def model(obs=None):
-        a = numpyro.sample('a', Normal(0, 1))
-        numpyro.sample('obs', Bernoulli(logits=a), obs=obs)
+        a = numpyro.sample("a", Normal(0, 1))
+        numpyro.sample("obs", Bernoulli(logits=a), obs=obs)
 
-    obs = Bernoulli(.5).sample(random.PRNGKey(0), (10,))
+    obs = Bernoulli(0.5).sample(random.PRNGKey(0), (10,))
     auto_guide = auto_class(model, init_loc_fn=init_loc_fn())
     with handlers.seed(rng_seed=0), handlers.trace() as auto_guide_tr:
         auto_guide(obs)
@@ -51,9 +66,9 @@ def test_auto_guide(auto_class, init_loc_fn, num_particles):
     for name, (init_value, constraint) in init_params.items():
         assert name in auto_guide_tr
         auto_param = auto_guide_tr[name]
-        assert init_value.shape == (num_particles, *auto_param['value'].shape)
+        assert init_value.shape == (num_particles, *auto_param["value"].shape)
 
-        if 'constraint' in auto_param['kwargs']:
-            assert constraint == auto_param['kwargs']['constraint']
+        if "constraint" in auto_param["kwargs"]:
+            assert constraint == auto_param["kwargs"]["constraint"]
         else:
             constraint == _Real()
