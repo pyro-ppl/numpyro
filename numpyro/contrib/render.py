f197e4fe2a9c9abc102d5f4e99a9bc4510f275f0
Matthew Aburn <mattja6@gmail.com>
2022-07-20 15:19:09 +1000

    retain contrib.render stub for backwards compatibility

---
+++ numpyro/contrib/render.py
@@ -0,0 +1,4 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+from numpyro.infer.inspect import get_model_relations, generate_graph_specification, render_graph, render_model
