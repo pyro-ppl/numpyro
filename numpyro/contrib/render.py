835f50cdaeb280b5c11c2a18f7da94ac33671403
Matthew Aburn <mattja6@gmail.com>
2022-07-22 11:58:59 +1000

    lint

--- numpyro/contrib/render.py
+++ numpyro/contrib/render.py
@@ -1,4 +1,16 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
-from numpyro.infer.inspect import get_model_relations, generate_graph_specification, render_graph, render_model
+from numpyro.infer.inspect import (
+    generate_graph_specification,
+    get_model_relations,
+    render_graph,
+    render_model,
+)
+
+__all__ = [
+    "generate_graph_specification",
+    "get_model_relations",
+    "render_graph",
+    "render_model",
+]
