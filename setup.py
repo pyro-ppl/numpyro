eaedbf6beb9da21430ef9fefced377508930ee9c
Hessam Mehr <hessammehr@gmail.com>
2022-10-25 15:37:11 +0000

    Constrain importlib-metadata version to fix CI.

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         "test": [
             "black[jupyter]>=21.8b0",
             "flake8",
+            "importlib-metadata<5.0",
             "isort>=5.0",
             "pytest>=4.1",
             "pyro-api>=0.1.1",
