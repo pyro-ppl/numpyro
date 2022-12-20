3c87b16a05b612dfd4566b5284fb7be6f492a393
Du Phan <phandu@google.com>
2022-12-19 20:11:08 -0500

    Attempt to not install dev version of black

--- numpyro/distributions/directional.py
+++ numpyro/distributions/directional.py
@@ -325,7 +325,7 @@ class SineBivariateVonMises(Distribution):
     :param np.ndarray phi_concentration: concentration of first angle
     :param np.ndarray psi_concentration: concentration of second angle
     :param np.ndarray correlation: correlation between the two angles
-    :param np.ndarray weighted_correlation: set correlation to weigthed_corr * sqrt(phi_conc*psi_conc)
+    :param np.ndarray weighted_correlation: set correlation to weighted_corr * sqrt(phi_conc*psi_conc)
         to avoid bimodality (see note). The `weighted_correlation` should be in [0,1].
     """
 
