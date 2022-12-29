f7b7080049c91a750bf0ec0bce893042d5fff6dc
Du Phan <fehiepsi@gmail.com>
2022-12-28 19:14:12 -0500

    Avoid comparing constraints positive and unit_interval (#1519)
    * Avoid comparing constraints positive and unit_interval
    
    * remove the bijective branch for positive constraint

--- numpyro/distributions/transforms.py
+++ numpyro/distributions/transforms.py
@@ -1067,8 +1067,6 @@ def _transform_to_corr_matrix(constraint):
 
 @biject_to.register(constraints.greater_than)
 def _transform_to_greater_than(constraint):
-    if constraint is constraints.positive:
-        return ExpTransform()
     return ComposeTransform(
         [
             ExpTransform(),
@@ -1097,8 +1095,6 @@ def _biject_to_independent(constraint):
 @biject_to.register(constraints.open_interval)
 @biject_to.register(constraints.interval)
 def _transform_to_interval(constraint):
-    if constraint is constraints.unit_interval:
-        return SigmoidTransform()
     scale = constraint.upper_bound - constraint.lower_bound
     return ComposeTransform(
         [
