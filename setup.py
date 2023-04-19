a6f90586551b8b6cf96064fcf5111acbcbaea9c9
Tarmo Äijö <506537+tare@users.noreply.github.com>
2023-04-19 15:24:47 -0400

    Modify scipy version restriction

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
             "isort>=5.0",
             "pytest>=4.1",
             "pyro-api>=0.1.1",
-            "scipy>=1.6",
+            "scipy>=1.6,<1.8",
         ],
         "dev": [
             "dm-haiku",
