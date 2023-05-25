27dcefc7ca7ab854c118eafe909d4b74b3a7cd79
OlaRonning <ronning@pm.me>
2023-05-25 10:12:05 +0200

    fixed default to cpu for stein_bnn.py

--- examples/stein_bnn.py
+++ examples/stein_bnn.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     parser.add_argument("--num-stein-particles", type=int, default=20)
     parser.add_argument("--progress-bar", type=bool, default=True)
     parser.add_argument("--rng-key", type=int, default=142)
-    parser.add_argument("--device", default="gpu", choices=["gpu", "cpu"])
+    parser.add_argument("--device", default="cpu", choices=["gpu", "cpu"])
     parser.add_argument("--hidden-dim", default=50, type=int)
 
     args = parser.parse_args()
