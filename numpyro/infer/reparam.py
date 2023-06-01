11818f9f998751f598ae7c4d4cef295d03cf052e
Madhav Kanda <76394914+Madhav-Kanda@users.noreply.github.com>
2023-06-01 19:26:46 +0530

    Making suggested changes

--- numpyro/infer/reparam.py
+++ numpyro/infer/reparam.py
@@ -72,7 +72,10 @@ class LocScaleReparam(Reparam):
     1. *Automatic Reparameterisation of Probabilistic Programs*,
        Maria I. Gorinova, Dave Moore, Matthew D. Hoffman (2019)
 
-    :param float centered: optional centered parameter. If None (default) considers centered parameter to be 0.5. If 0, fully
+    :param float centered: optional centered parameter. If None (default) learn
+        a per-site per-element centering parameter in ``[0,1]`` initialized at value 0.5.
+        To sample the parameter, consider using :class:`~numpyro.handlers.lift` handler with a
+        prior like ``Uniform(0, 1)`` to cast the parameter to a latent variable. If 0, fully
         decenter the distribution; if 1, preserve the centered distribution
         unchanged.
     :param shape_params: list of additional parameter names to copy unchanged from
