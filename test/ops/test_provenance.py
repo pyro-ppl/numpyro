59a188dfb443b736d9f68f9042deef5102bf8c2e
Du Phan <fehiepsi@gmail.com>
2023-09-22 18:46:22 -0400

    avoid breakage in old jax version without jax.extend (#1647)
    * avoid breakage in old jax version without jax.extend
    
    * fix lint

--- test/ops/test_provenance.py
+++ test/ops/test_provenance.py
@@ -8,7 +8,11 @@ import pytest
 import jax
 from jax.api_util import flatten_fun_nokwargs
 import jax.core as core
-import jax.extend.linear_util as lu
+
+try:
+    import jax.extend.linear_util as lu
+except ImportError:
+    import jax.linear_util as lu
 import jax.numpy as jnp
 
 from numpyro.ops.provenance import eval_provenance
