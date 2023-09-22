6e3f00774d7f79b53008c332bc9d2e68c8702f24
Du Phan <fehiepsi@gmail.com>
2023-09-22 05:55:21 -0400

    Bump to 0.13.1 (#1643)
    * Bump to 0.13.1
    
    * fix lint

--- examples/funnel.py
+++ examples/funnel.py
@@ -139,7 +139,7 @@ def main(args):
 
 
 if __name__ == "__main__":
-    assert numpyro.__version__.startswith("0.13.0")
+    assert numpyro.__version__.startswith("0.13.1")
     parser = argparse.ArgumentParser(
         description="Non-centered reparameterization example"
     )
