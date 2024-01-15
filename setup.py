56e04e736629adc87c875812518990d6abe970f1
juanitorduz <juanitorduz@gmail.com>
2024-01-15 17:51:02 +0100

    add pin in setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,8 @@ setup(
             "ipython",  # sphinx needs this to render codes
             "nbsphinx>=0.8.5",
             "readthedocs-sphinx-search==0.1.0",
-            "sphinx",
+            "sphinxcontrib-applehelp<1.0.8"
+            "sphinx<5",
             "sphinx_rtd_theme",
             "sphinx-gallery",
         ],
