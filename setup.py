17bffc48e07162ee69d3808024f7384746bc696e
Du Phan <phandu@google.com>
2024-01-16 12:02:32 -0500

    use latest sphinx search extension for security

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ setup(
     extras_require={
         "doc": [
             "ipython",  # sphinx needs this to render codes
-            "nbsphinx>=0.8.5",
-            "readthedocs-sphinx-search",
+            "nbsphinx>=0.8.9",
+            "readthedocs-sphinx-search>=0.3.2",
             "sphinx>=5",
             "sphinx_rtd_theme",
             "sphinx-gallery",
