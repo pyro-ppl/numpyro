d738dd5629251f1496df4f5291835e50ec4a8fc5
Till Hoffmann <tillahoffmann@gmail.com>
2024-03-15 15:02:20 -0400

    Remove `isreal` check.

--- numpyro/distributions/constraints.py
+++ numpyro/distributions/constraints.py
@@ -642,7 +642,7 @@ class _Complex(_SingletonConstraint):
 class _Real(_SingletonConstraint):
     def __call__(self, x):
         # XXX: consider to relax this condition to [-inf, inf] interval
-        return (x == x) & (x != float("inf")) & (x != float("-inf")) & np.isreal(x)
+        return (x == x) & (x != float("inf")) & (x != float("-inf"))
 
     def feasible_like(self, prototype):
         return jax.numpy.zeros_like(prototype)
