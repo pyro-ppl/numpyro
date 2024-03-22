fb197f3d2f7134cddbe536d3d286b9bc4582613e
Till Hoffmann <tillahoffmann@gmail.com>
2024-03-21 20:22:47 -0400

    Use non-trivial transition matrix in test.

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -284,7 +284,10 @@ def test_real_fast_fourier_transform(input_shape, shape, ndims):
         (PowerTransform(2.5), ()),
         (RealFastFourierTransform(7), (7,)),
         (RealFastFourierTransform((8, 9), 2), (8, 9)),
-        (RecursiveLinearTransform(jnp.eye(4)), (10, 4)),
+        (
+            RecursiveLinearTransform(random.normal(random.key(17), (4, 4))),
+            (7, 4),
+        ),
         (ReshapeTransform((5, 2), (10,)), (10,)),
         (ReshapeTransform((15,), (3, 5)), (3, 5)),
         (ScaledUnitLowerCholeskyTransform(), (6,)),
