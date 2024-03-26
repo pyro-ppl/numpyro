8cb7a5fd0dbcd69062628905056a18f23cfec9a1
Kyle Caron <kyle.j.caron@gmail.com>
2024-03-26 15:04:30 -0400

    removed list from ZeroSum constraint call

--- numpyro/distributions/constraints.py
+++ numpyro/distributions/constraints.py
@@ -705,9 +705,9 @@ class _ZeroSum(Constraint):
 
     def __call__(self, x):
         jnp = np if isinstance(x, (np.ndarray, np.generic)) else jax.numpy
-        zerosum_true = []
+        zerosum_true = True
         for dim in range(-self.event_dim, 0):
-            zerosum_true.append(jnp.allclose(x.sum(dim), 0, rtol=0.05, atol=1e-2))
+            zerosum_true = zerosum_true & jnp.allclose(x.sum(dim), 0, rtol=0.05, atol=1e-2)
         return all(zerosum_true)
 
     def __eq__(self, other):
