ddcff5a01ee31cef885f51d3ffb51df212b9a94c
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-03-31 22:08:01 +0500

    test: update test case for Dagum distribution parameters

--- test/test_distributions.py
+++ test/test_distributions.py
@@ -962,7 +962,7 @@ CONTINUOUS = [
     T(dist.Dagum, 1.0, 1.0, 1.0),
     T(dist.Dagum, 3.0, 4.0, 5.0),
     T(dist.Dagum, 2.0, np.array([1.0, 2.0, 10.0]), 4.0),
-    T(dist.Dagum, 2.0, 3.0, np.array([5.0, 2.0, 10.0])),
+    T(dist.Dagum, 2.0, 3.0, np.array([4.0, 2.0, 10.0])),
 ]
 
 DIRECTIONAL = [
