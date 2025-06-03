fix(test): adjust atol parameter for variance assertion in `test_mean_var`

test/test_distributions.py
@@ -2089,7 +2089,7 @@ def test_mean_var(jax_dist, sp_dist, params):
             pytest.skip("Gompertz distribution does not have `variance` implemented.")
         if jnp.all(jnp.isfinite(d_jax.variance)):
             assert jnp.allclose(
-                jnp.std(samples, 0), jnp.sqrt(d_jax.variance), rtol=0.05, atol=1e-2
+                jnp.std(samples, 0), jnp.sqrt(d_jax.variance), rtol=0.05, atol=0.05
             )
 
 