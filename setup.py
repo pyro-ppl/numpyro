fde3b4115c36e7be41db17dbf077c93b5c620514
Meesum Qazalbash <meesumqazalbash@gmail.com>
2026-02-15 12:58:51 +0500

    Revert "chore: `setup.py` -> `pyproject.toml`"
    This reverts commit c3ce49fddde380b18fc9af270b70d0c3fe01fc37.

---
+++ setup.py
@@ -0,0 +1,109 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+_jax_version_constraints = ">=0.7.0"
+_jaxlib_version_constraints = ">=0.7.0"
+
+# Find version
+for line in open(os.path.join(PROJECT_PATH, "numpyro", "version.py")):
+    if line.startswith("__version__ = "):
+        version = line.strip().split()[2][1:-1]
+
+# READ README.md for long description on PyPi.
+try:
+    long_description = open("README.md", encoding="utf-8").read()
+except Exception as e:
+    sys.stderr.write("Failed to read README.md:\n  {}\n".format(e))
+    sys.stderr.flush()
+    long_description = ""
+
+setup(
+    name="numpyro",
+    version=version,
+    description="Pyro PPL on NumPy",
+    packages=find_packages(include=["numpyro", "numpyro.*"]),
+    url="https://github.com/pyro-ppl/numpyro",
+    author="Uber AI Labs",
+    install_requires=[
+        f"jax{_jax_version_constraints}",
+        f"jaxlib{_jaxlib_version_constraints}",
+        "multipledispatch",
+        "numpy",
+        "tqdm",
+    ],
+    extras_require={
+        "doc": [
+            "ipython",  # sphinx needs this to render codes
+            "nbsphinx>=0.8.9",
+            "readthedocs-sphinx-search>=0.3.2",
+            "sphinx>=5",
+            "sphinx_rtd_theme",
+            "sphinx-gallery",
+        ],
+        "test": [
+            "importlib-metadata<5.0",
+            "ruff>=0.1.8",
+            "mypy>=1.13",
+            "pytest>=4.1",
+            "pyro-api>=0.1.1",
+            "scikit-learn",
+            "scipy>=1.9",
+            "ty>=0.0.4",
+        ],
+        "dev": [
+            "dm-haiku>=0.0.14",
+            "equinox",
+            "flax",
+            "funsor>=0.4.1",
+            "graphviz",
+            "jaxns>=2.6.3,<=2.6.9",
+            "matplotlib",
+            "optax>=0.0.6",
+            "pylab-sdk",  # jaxns dependency
+            "pytest-cov",
+            "pyyaml",  # flax dependency
+            "requests",  # pylab dependency
+            "tfp-nightly",
+        ],
+        "examples": [
+            "arviz",
+            "jupyter",
+            "matplotlib",
+            "pandas",
+            "seaborn",
+            "scikit-learn",
+            "wordcloud",
+        ],
+        "cpu": f"jax[cpu]{_jax_version_constraints}",
+        # TPU and CUDA installations, currently require to add package repository URL, i.e.,
+        # pip install 'numpyro[cuda]' -f https://storage.googleapis.com/jax-releases/jax_releases.html
+        "tpu": f"jax[tpu]{_jax_version_constraints}",
+        "cuda12": f"jax[cuda12]{_jax_version_constraints}",
+        "cuda13": f"jax[cuda13]{_jax_version_constraints}",
+    },
+    python_requires=">=3.9",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="probabilistic machine learning bayesian statistics",
+    license="Apache License 2.0",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+    ],
+)
