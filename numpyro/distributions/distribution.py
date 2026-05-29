955d50f63006992b562be1309a75d089c81d6443
Simon Dupourqué <49200287+renecotyfanboy@users.noreply.github.com>
2026-05-29 11:22:37 +0200

    Revert "set default to True"
    This reverts commit c66e2fbb034fde8df928c50313f76da4ce4f118b.

--- numpyro/distributions/distribution.py
+++ numpyro/distributions/distribution.py
@@ -64,7 +64,7 @@ from numpyro.util import find_stack_level, not_jax_tracer
 
 from . import constraints
 
-_VALIDATION_ENABLED = True
+_VALIDATION_ENABLED = False
 
 
 def enable_validation(is_validate: bool = True) -> None:
