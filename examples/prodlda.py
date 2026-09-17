ad7481896b65963a284ff14aeb1985d3903f4d35
Juan Orduz <juanitorduz@gmail.com>
2026-09-17 19:57:37 +0200

    run script

--- examples/prodlda.py
+++ examples/prodlda.py
@@ -229,7 +229,7 @@ def main(args):
 
 
 if __name__ == "__main__":
-    assert numpyro.__version__.startswith("0.21.0")
+    assert numpyro.__version__.startswith("0.22.0")
     parser = argparse.ArgumentParser(
         description="Probabilistic topic modelling with Flax"
     )
